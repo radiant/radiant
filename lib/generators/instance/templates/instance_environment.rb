336d67c86069c40e1de994a7c09a519faa24569b
Josh French <josh@digitalpulp.com>
2008-06-27 12:30:16 -0400

    Update instance generator to use cookie_store

--- lib/generators/instance/templates/instance_environment.rb
+++ lib/generators/instance/templates/instance_environment.rb
@@ -45,7 +45,7 @@ Radiant::Initializer.run do |config|
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
-  config.action_controller.session_store = :active_record_store
+  config.action_controller.session_store = :cookie_store
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
