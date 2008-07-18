e70f3b6c56f10db483f684c2b7f661d91d083d1c
Sean Cribbs <seancribbs@gmail.com>
2008-07-18 15:06:44 -0400

    Add extension installation scripts.

---
+++ lib/radiant/extension/script.rb
@@ -0,0 +1,229 @@
+require 'active_resource'
+require 'tmpdir'
+require 'fileutils'
+
+module Registry
+  class Extension < ActiveResource::Base
+    self.site = ENV['REGISTRY_URL'] || "http://ext.radiantcms.org/"
+
+    def install
+      install_type.constantize.new(self).install
+    end
+
+    def uninstall
+      Uninstaller.new(self).uninstall
+    end
+  end
+
+  class Action
+    def rake(command)
+      `rake #{command} RAILS_ENV=#{RAILS_ENV}`
+    end
+  end
+
+  class Installer < Action
+    attr_accessor :url, :path, :name
+    def initialize(url, name)
+      self.url, self.name = url, name
+    end
+
+    def install
+      copy_to_vendor_extensions
+      migrate
+      update
+    end
+
+    def copy_to_vendor_extensions
+      FileUtils.cp_r(self.path, File.expand_path(File.join(RAILS_ROOT, 'vendor', 'extensions', name)))
+      FileUtils.rm_r(self.path)
+    end
+
+    def migrate
+      rake "radiant:extensions:#{name}:migrate"
+    end
+
+    def update
+      rake "radiant:extensions:#{name}:update"
+    end
+  end
+
+  class Uninstaller < Action
+    attr_accessor :name
+    def initialize(extension)
+      self.name = extension.name
+    end
+
+    def uninstall
+      migrate_down
+      remove_extension_directory
+      cleanup_environment
+    end
+
+    def migrate_down
+      rake "radiant:extensions:#{name}:migrate VERSION=0"
+    end
+
+    def remove_extension_directory
+      FileUtils.rm_r(File.join(RAILS_ROOT, 'vendor', 'extensions', name))
+    end
+
+    def cleanup_environment
+      # Maybe in the future clear it out of the config.extensions array
+    end
+  end
+
+  class Checkout < Installer
+    def initialize(extension)
+      super(extension.repository_url, extension.name)
+    end
+
+    def checkout_command
+      raise "Not Implemented!"
+    end
+
+    def install
+      checkout
+      super
+    end
+
+    def checkout
+      self.path = File.join(Dir.tmpdir, name)
+      system "cd #{Dir.tmpdir}; #{checkout_command}"
+    end
+  end
+
+  class Download < Installer
+    def initialize(extension)
+      super(extension.download_url, extension.name)
+    end
+
+    def install
+      download
+      unpack
+      super
+    end
+
+    def unpack
+      raise "Not Implemented!"
+    end
+
+    def filename
+      File.basename(self.url)
+    end
+
+    def download
+      require 'open-uri'
+      File.open(File.join(Dir.tmpdir, self.filename), 'w') {|f| f.write open(self.url).read }
+    end
+  end
+
+  class Git < Checkout
+    def checkout_command
+      "git clone #{url} #{name}"
+    end
+  end
+
+  class Subversion < Checkout
+    def checkout_command
+      "svn checkout #{url} #{name}"
+    end
+  end
+
+  class Gem < Download
+    def download
+      # Don't download the gem if it's already installed
+      begin
+        gem filename.split('-').first
+      rescue ::Gem::LoadError
+        super
+        `gem install #{filename}`
+      end
+    end
+
+    def unpack
+      output = `cd #{Dir.tmpdir}; gem unpack #{filename.split('-').first}`
+      self.path = output.match(/'(.*)'/)[1]
+    end
+  end
+
+  class Tarball < Download
+    def unpack
+      packed  = filename =~ /gz/ ? 'z' : ''
+      output = `cd #{Dir.tmpdir}; tar xvf#{packed} #{filename}`
+      self.path = File.join(Dir.tmpdir, output.split(/\n/).first.split('/').first)
+    end
+  end
+
+  class Zip < Download
+    def unpack
+      output = `cd #{Dir.tmpdir}; unzip #{filename} -d #{name}`
+      self.path = File.join(Dir.tmpdir, name)
+    end
+  end
+end
+
+module Radiant
+  class Extension
+    module Script
+      class << self
+        def execute(args)
+          command = args.shift
+          const_get(command.camelize).new(args)
+        end
+      end
+
+      module Util
+        attr_accessor :extension_name, :extension
+
+        def to_extension_name(string)
+          string.to_s.underscore
+        end
+
+        def installed?
+          path_match = Regexp.compile("#{extension_name}$")
+          extension_paths.any? {|p| p =~ path_match }
+        end
+
+        def extension_paths
+          [RAILS_ROOT, RADIANT_ROOT].uniq.map { |p| Dir["#{p}/vendor/extensions/*"] }.flatten
+        end
+
+        def load_extensions
+          Registry::Extension.find(:all)
+        end
+
+        def find_extension
+          self.extension = load_extensions.find{|e| e.name == self.extension_name }
+        end
+      end
+
+      class Install
+        include Util
+
+        def initialize(args=[])
+          raise ArgumentError, "You must specify an extension to install." if args.blank?
+          self.extension_name = to_extension_name(args.shift)
+          if installed?
+            puts "#{extension_name} is already installed."
+          else
+            find_extension && extension.install
+          end
+        end
+      end
+
+      class Uninstall
+        include Util
+
+        def initialize(args=[])
+          raise ArgumentError, "You must specify an extension to uninstall." if args.blank?
+          self.extension_name = to_extension_name(args.shift)
+          if installed?
+            find_extension && extension.uninstall
+          else
+            puts "#{extension} is not installed."
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/radiant/extension/script_spec.rb
@@ -0,0 +1,332 @@
+require File.dirname(__FILE__) + "/../../../spec_helper"
+require 'radiant/extension/script'
+
+describe "Radiant::Extension::Script" do
+  it "should determine which subscript to run" do
+    Radiant::Extension::Script::Install.should_receive(:new)
+    Radiant::Extension::Script.execute ['install']
+
+    Radiant::Extension::Script::Uninstall.should_receive(:new)
+    Radiant::Extension::Script.execute ['uninstall']
+  end
+
+  it "should pass the command-line args to the subscript" do
+    Radiant::Extension::Script::Install.should_receive(:new).with(['page_attachments'])
+    Radiant::Extension::Script.execute ['install', 'page_attachments']
+  end
+end
+
+describe "Radiant::Extension::Script::Util" do
+  include Radiant::Extension::Script::Util
+
+  it "should determine an extension name from a camelized string" do
+    to_extension_name("PageAttachments").should == 'page_attachments'
+  end
+
+  it "should determine an extension name from a hyphened name" do
+    to_extension_name("page-attachments").should == 'page_attachments'
+  end
+
+  it "should determine an extension name from an underscored name" do
+    to_extension_name("page_attachments").should == 'page_attachments'
+  end
+
+  it "should determine extension paths" do
+    # Bad coupling, but will work by default
+    extension_paths.should be_kind_of(Array)
+    extension_paths.should include("#{RADIANT_ROOT}/vendor/extensions/archive")
+  end
+
+  it "should determine whether an extension is installed" do
+    # Bad coupling, but will work by default
+    self.extension_name = 'archive'
+    self.should be_installed
+  end
+
+  it "should load all extensions from the web service" do
+    Registry::Extension.should_receive(:find).with(:all).and_return([1,2,3])
+    load_extensions.should == [1,2,3]
+  end
+
+  it "should find an extension of the given name from the web service" do
+    @ext_mock = mock("Extension", :name => 'page_attachments')
+    should_receive(:load_extensions).and_return([@ext_mock])
+    self.extension_name = 'page_attachments'
+    find_extension.should == @ext_mock
+  end
+end
+
+describe "Radiant::Extension::Script::Install" do
+
+  before :each do
+    @extension = mock('Extension', :install => true, :name => 'page_attachments')
+    Registry::Extension.stub!(:find).and_return([@extension])
+  end
+
+  it "should read the extension name from the command line" do
+    @install = Radiant::Extension::Script::Install.new ['page_attachments']
+    @install.extension_name.should == 'page_attachments'
+  end
+
+  it "should attempt to find the extension and install it" do
+    @extension.should_receive(:install).and_return(true)
+    @install = Radiant::Extension::Script::Install.new ['page_attachments']
+  end
+
+  it "should fail if an extension name is not given" do
+    lambda { Radiant::Extension::Script::Install.new []}.should raise_error
+  end
+end
+
+describe "Radiant::Extension::Script::Uninstall" do
+
+  before :each do
+    @extension = mock('Extension', :uninstall => true, :name => 'archive')
+    Registry::Extension.stub!(:find).and_return([@extension])
+  end
+
+  it "should read the extension name from the command line" do
+    @uninstall = Radiant::Extension::Script::Uninstall.new ['archive']
+    @uninstall.extension_name.should == 'archive'
+  end
+
+  it "should attempt to find the extension and uninstall it" do
+    @extension.should_receive(:uninstall).and_return(true)
+    @uninstall = Radiant::Extension::Script::Uninstall.new ['archive']
+  end
+
+  it "should fail if an extension name is not given" do
+    lambda { Radiant::Extension::Script::Uninstall.new []}.should raise_error
+  end
+end
+
+describe "Registry::Action" do
+  before :each do
+    @action = Registry::Action.new
+  end
+
+  it "should shell out with the specified rake task" do
+    @action.should_receive(:`).with("rake sample RAILS_ENV=#{RAILS_ENV}")
+    @action.rake('sample')
+  end
+end
+
+describe "Registry::Installer" do
+  before :each do
+    @installer = Registry::Installer.new('http://localhost/', 'example')
+  end
+
+  it "should set the name and url of the extension" do
+    @installer.url.should == 'http://localhost/'
+    @installer.name.should == 'example'
+  end
+
+  it "should install by copying, migrating and updating" do
+    @installer.should_receive(:copy_to_vendor_extensions)
+    @installer.should_receive(:migrate)
+    @installer.should_receive(:update)
+    @installer.install
+  end
+
+  it "should copy the extension to vendor/extensions" do
+    @installer.path = "/tmp"
+    FileUtils.should_receive(:cp_r).with('/tmp', "#{RAILS_ROOT}/vendor/extensions/example")
+    FileUtils.should_receive(:rm_r).with('/tmp')
+    @installer.copy_to_vendor_extensions
+  end
+
+  it "should run the rake migrate task" do
+    @installer.should_receive(:rake).with('radiant:extensions:example:migrate')
+    @installer.migrate
+  end
+
+  it "should run the rake update task" do
+    @installer.should_receive(:rake).with('radiant:extensions:example:update')
+    @installer.update
+  end
+end
+
+describe "Registry::Uninstaller" do
+  before :each do
+    @extension = mock('Extension', :name => 'example')
+    @uninstaller = Registry::Uninstaller.new(@extension)
+  end
+
+  it "should migrate down" do
+    @uninstaller.should_receive(:rake).with("radiant:extensions:example:migrate VERSION=0")
+    @uninstaller.migrate_down
+  end
+
+  it "should remove the extension directory" do
+    FileUtils.should_receive(:rm_r).with("#{RAILS_ROOT}/vendor/extensions/example")
+    @uninstaller.remove_extension_directory
+  end
+
+  it "should uninstall by migrating down, removing the directory and cleaning up environment" do
+    @uninstaller.should_receive(:migrate_down)
+    @uninstaller.should_receive(:remove_extension_directory)
+    @uninstaller.should_receive(:cleanup_environment)
+    @uninstaller.uninstall
+  end
+
+  it "should cleanup the environment" do
+    pending
+  end
+end
+
+describe "Registry::Checkout" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :repository_url => 'http://localhost/')
+    @checkout = Registry::Checkout.new(@extension)
+    @methods = [:copy_to_vendor_extensions, :migrate, :update].each do |method|
+      @checkout.stub!(method).and_return(true)
+    end
+  end
+
+  it "should set the name and url" do
+    @checkout.name.should == 'example'
+    @checkout.url.should == 'http://localhost/'
+  end
+
+  it "should defer the checkout command to concrete subclasses" do
+    lambda { @checkout.checkout_command }.should raise_error
+  end
+
+  it "should install by checking out the source and then proceeding with the normal installation" do
+    @methods.each { |method|  @checkout.should_receive(method) }
+    @checkout.should_receive(:checkout)
+    @checkout.install
+  end
+
+  it "should checkout the source" do
+    @checkout.stub!(:checkout_command).and_return('echo')
+    @checkout.should_receive(:system).with(/^cd (.*); echo/)
+    @checkout.checkout
+    @checkout.path.should_not be_nil
+    @checkout.path.should =~ /example/
+  end
+end
+
+describe "Registry::Download" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :download_url => 'http://localhost/example.pkg')
+    @download = Registry::Download.new(@extension)
+    @methods = [:copy_to_vendor_extensions, :migrate, :update].each do |method|
+      @download.stub!(method).and_return(true)
+    end
+  end
+
+  it "should set the name and url" do
+    @download.name.should == 'example'
+    @download.url.should == 'http://localhost/example.pkg'
+  end
+
+  it "should defer the unpack command to concrete subclasses" do
+    lambda { @download.unpack }.should raise_error
+  end
+
+  it "should install by downloading and unpacking and then proceeding with the normal installation" do
+    @methods.each { |method|  @download.should_receive(method) }
+    @download.should_receive(:download)
+    @download.should_receive(:unpack)
+    @download.install
+  end
+
+  it "should determine the filename" do
+    @download.filename.should == 'example.pkg'
+  end
+
+  it "should download the file" do
+    @file = mock('file')
+    File.should_receive(:open).with(/example\.pkg/, 'w').and_yield(@file)
+    @download.should_receive(:open).and_return(StringIO.new('test'))
+    @file.should_receive(:write).with('test')
+    @download.download
+  end
+end
+
+describe "Registry::Git" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :repository_url => 'http://localhost/')
+    @git = Registry::Git.new(@extension)
+  end
+
+  it "should use git to clone the repository" do
+    @git.checkout_command.should == 'git clone http://localhost/ example'
+  end
+end
+
+describe "Registry::Subversion" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :repository_url => 'http://localhost/')
+    @svn = Registry::Subversion.new(@extension)
+  end
+
+  it "should use svn to checkout the repository" do
+    @svn.checkout_command.should == 'svn checkout http://localhost/ example'
+  end
+end
+
+describe "Registry::Gem" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :download_url => 'http://localhost/example-1.0.0.gem')
+    @gem = Registry::Gem.new(@extension)
+  end
+
+  it "should download the gem and install it if it is not already installed" do
+    @gem.should_receive(:gem).and_raise(::Gem::LoadError.new)
+    @file = mock('file')
+    File.should_receive(:open).with(/example-1.0.0\.gem/, 'w').and_yield(@file)
+    @gem.should_receive(:open).and_return(StringIO.new('test'))
+    @file.should_receive(:write).with('test')
+    @gem.should_receive(:`).with("gem install example-1.0.0.gem")
+    @gem.download
+  end
+
+  it "should not download the gem if it is already installed" do
+    @gem.should_receive(:gem).and_return(true)
+    File.should_not_receive(:open)
+    @gem.should_not_receive(:open)
+    @gem.should_not_receive(:`)
+    @gem.download
+  end
+
+  it "should unpack the gem and capture the path" do
+    @gem.should_receive(:`).with(/gem unpack example/).and_return("Unpacked gem: '/tmp/example-1.0.0'")
+    @gem.unpack
+    @gem.path.should == '/tmp/example-1.0.0'
+  end
+end
+
+describe "Registry::Tarball" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :download_url => 'http://localhost/example-1.0.0.tar.gz')
+    @tar = Registry::Tarball.new(@extension)
+  end
+
+  it "should unpack the tarball with compression" do
+    @tar.should_receive(:`).with(/tar xvfz example-1.0.0.tar.gz/).and_return('example-1.0.0/example_extension.rb\n')
+    @tar.unpack
+    @tar.path.should =~ /example-1\.0\.0$/
+  end
+
+  it "should unpack the tarball without compression" do
+    @tar.url = @tar.url.sub(/.gz$/, '')
+    @tar.should_receive(:`).with(/tar xvf example-1.0.0.tar/).and_return('example-1.0.0/example_extension.rb\n')
+    @tar.unpack
+    @tar.path.should =~ /example-1\.0\.0$/
+  end
+end
+
+describe "Registry::Zip" do
+  before :each do
+    @extension = mock("Extension", :name => 'example', :download_url => 'http://localhost/example-1.0.0.zip')
+    @zip = Registry::Zip.new(@extension)
+  end
+
+  it "should unpack the zip" do
+    @zip.should_receive(:`).with(/unzip example-1.0.0.zip -d example/).and_return('')
+    @zip.unpack
+    @zip.path.should =~ /example$/
+  end
+end
