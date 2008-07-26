d749f6197681dab45f9f04c195afc5c0ca824f23
Jim Gay <jim@saturnflyer.com>
2008-07-25 23:58:52 -0400

    clearing rspec

--- vendor/plugins/rspec/stories/example_groups/stories.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), *%w[.. helper])
-
-with_steps_for :running_rspec do
-  run File.dirname(__FILE__) + "/nested_groups"
-  # Dir["#{File.dirname(__FILE__)}/*"].each do |file|
-  #   run file if File.file?(file) && !(file =~ /\.rb$/)
-  # end
-end
