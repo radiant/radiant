1620dac342f41f91821754eb662b507f1ea479f5
Josh French <josh@vitamin-j.com>
2008-08-17 14:07:59 -0400

    Update timezone conversion

--- lib/local_time.rb
+++ lib/local_time.rb
@@ -4,7 +4,7 @@ module LocalTime
     if (tz_string = Radiant::Config["local.timezone"]) and 
         timezone = (TimeZone[tz_string] || TimeZone[tz_string.to_i]) 
       # adjust time 
-      timezone.adjust(time)
+      time.in_time_zone(timezone).time
     else 
       time 
     end 
