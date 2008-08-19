f39d9ba3f47061dfe56d56ddc799a498c502aa39
Jim Gay <jim@saturnflyer.com>
2008-08-18 20:15:56 -0400

    updated rspec syntax for node_helper_spec

--- spec/helpers/admin/node_helper_spec.rb
+++ spec/helpers/admin/node_helper_spec.rb
@@ -13,15 +13,17 @@ describe Admin::NodeHelper do
   it "should render a sitemap node" do
     helper.should_receive(:render).with(:partial => "node", :locals => {:level => 0, :simple => false, :page => @page}).and_return(@current_node)
     helper.render_node(@page)
-    @current_node.should == @page
+    assigns[:current_node] == @page
   end
 
   it "should show all nodes when on the remove action" do
+    assigns[:controller] = @controller
     @controller.should_receive(:action_name).and_return("remove")
     helper.show_all?.should be_true
   end
 
   it "should not show all nodes automatically when not in the remove action" do
+    assigns[:controller] = @controller
     @controller.should_receive(:action_name).and_return("index")
     helper.show_all?.should be_false
   end
@@ -43,7 +45,7 @@ describe Admin::NodeHelper do
   end
 
   it "should determine the class of a parent node" do
-    @current_node = @page
+    assigns[:current_node] = @page
     child = mock("child")
     @page.should_receive(:children).and_return([child])
     helper.should_receive(:expanded).and_return(true)
@@ -51,27 +53,27 @@ describe Admin::NodeHelper do
   end
 
   it "should display an icon for the current node" do
-    @current_node = @page
+    assigns[:current_node] = @page
     @page.should_receive(:virtual?).and_return(false)
     helper.should_receive(:image).with("page", :class => "icon", :alt => 'page-icon', :title => '')
     helper.icon
   end
   
   it "should display the virtual icon if the current node is virtual" do
-    @current_node = @page
+    assigns[:current_node] = @page
     @page.should_receive(:virtual?).and_return(true)
     helper.should_receive(:image).with("virtual-page", :class => "icon", :alt => 'page-icon', :title => '')
     helper.icon
   end
 
   it "should render the title of the current node" do
-    @current_node = @page
+    assigns[:current_node] = @page
     @page.should_receive(:title).and_return("Title")
     helper.node_title.should == %{<span class="title">Title</span>}
   end
 
   it "should render the page type if it's not Page" do
-    @current_node = @page
+    assigns[:current_node] = @page
     @class = mock("Class")
     @page.should_receive(:class).and_return(@class)
     @class.should_receive(:display_name).and_return("Special")
@@ -79,13 +81,13 @@ describe Admin::NodeHelper do
   end
 
   it "should not render the page type if it's Page" do
-    @current_node = @page
+    assigns[:current_node] = @page
     @page.should_receive(:class).and_return(Page)
     helper.page_type.should ==  %{}
   end
 
   it "should render the busy spinner" do
-    @current_node = @page
+    assigns[:current_node] = @page
     @page.should_receive(:id).and_return(1)
     helper.should_receive(:image).with('spinner.gif',
             :class => 'busy', :id => "busy-1",
