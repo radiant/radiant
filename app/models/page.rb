bfa1e8ff512da17f57c72dd6b8537ade3560c147
Jim Gay <jim@saturnflyer.com>
2008-09-08 13:31:34 -0400

    Revert "Check for the"
    This reverts commit 62eb1d0ea46b92b442bc94e98c8bc8eb5bba0883.

--- app/models/page.rb
+++ app/models/page.rb
@@ -191,7 +191,7 @@ class Page < ActiveRecord::Base
           $1.camelize.constantize if page =~ %r{/([^/]+)\.rb}
         end
       end
-      if ActiveRecord::Base.connection.tables.include?('pages') # Haven't bootstrapped yet
+      unless Page.connection.tables.empty? # Haven't bootstrapped yet
         Page.connection.select_values("SELECT DISTINCT class_name FROM pages WHERE class_name <> '' AND class_name IS NOT NULL").each do |p|
           begin
             p.constantize
