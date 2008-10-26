2dd8c447d896eb4b5da463e4547a1f2ece8ef37f
Andrew O'Brien <obrien.andrew@gmail.com>
2008-10-25 21:01:33 -0400

    Added some friendly aliases to String#to_slug

--- lib/plugins/string_extensions/lib/string_extensions.rb
+++ lib/plugins/string_extensions/lib/string_extensions.rb
@@ -10,6 +10,13 @@ module StringExtensions
   def to_name(last_part = '')
     self.underscore.gsub('/', ' ').humanize.titlecase.gsub(/\s*#{last_part}$/, '')
   end
+  
+  def to_slug
+    self.strip.downcase.gsub(/[^-a-z0-9~\s\.:;+=_]/, '').gsub(/[\s\.:;=+]+/, '-')
+  end
+  
+  alias_method :slugify, :to_slug
+  alias_method :slugerize, :to_slug
 end
 
 String.send :include, StringExtensions
