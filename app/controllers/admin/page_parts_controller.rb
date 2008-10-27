03d7e4936969617d0b818682c8551538e6399dcb
Sean Cribbs <seancribbs@gmail.com>
2008-10-27 14:31:52 -0400

    Fix add-part workflow.

--- app/controllers/admin/page_parts_controller.rb
+++ app/controllers/admin/page_parts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PagePartsController < Admin::ResourceController
   def create
     self.model.attributes = params[model_symbol]
     @controller_name = 'page'
-    @action_name = 'edit'
+    @template_name = 'edit'
     render :partial => "page_part", :object => model,
       :locals => {:index => params[:index].to_i}
   end
