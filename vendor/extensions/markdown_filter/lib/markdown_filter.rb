028946a3ab348390a4735ad220bc111afb95b1e5
john <git@johnmuhl.com>
2009-01-29 19:42:56 -0500

    Use RDiscount's smart quotes instead of SmartyPants.

--- vendor/extensions/markdown_filter/lib/markdown_filter.rb
+++ vendor/extensions/markdown_filter/lib/markdown_filter.rb
@@ -1,6 +1,10 @@
 class MarkdownFilter < TextFilter
   description_file File.dirname(__FILE__) + "/../markdown.html"
   def filter(text)
-    RubyPants.new(BlueCloth.new(text).to_html).to_html
+    if defined? RDiscount
+      RDiscount.new(text, :smart).to_html
+    else
+      RubyPants.new(BlueCloth.new(text).to_html).to_html
+    end
   end
 end
