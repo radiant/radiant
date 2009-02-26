818ae3f91f2e1c095db9452960e11638d06d6f2c
Sean Cribbs <seancribbs@gmail.com>
2009-02-25 21:53:21 -0500

    Correct rendering error in extensions controller.

--- app/controllers/admin/extensions_controller.rb
+++ app/controllers/admin/extensions_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ExtensionsController < ApplicationController
   def index
+    @template_name = 'index' # for Admin::RegionsHelper
     @extensions = Radiant::Extension.descendants.sort_by { |e| e.extension_name }
   end
 end
--- spec/controllers/admin/extensions_controller_spec.rb
+++ spec/controllers/admin/extensions_controller_spec.rb
@@ -2,20 +2,35 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Admin::ExtensionsController do
   dataset :users
-  
+
   before :each do
     login_as :admin
   end
-  
+
   it "should require login for all actions" do
     logout
     lambda { get :index }.should require_login
   end
-  
-  it "should list all extensions" do
-    get :index
-    response.should be_success
-    response.should render_template("index")
-    assigns[:extensions].should == Radiant::Extension.descendants.sort_by(&:name)
+
+  describe "GET to /admin/extensions" do
+    before :each do
+      get :index
+    end
+
+    it "should be successful" do
+      response.should be_success
+    end
+
+    it "should render the index template" do
+      response.should render_template("index")
+    end
+
+    it "should list all extensions" do
+      assigns[:extensions].should == Radiant::Extension.descendants.sort_by(&:name)
+    end
+
+    it "should pre-set the template name" do
+      assigns[:template_name].should == 'index'
+    end
   end
 end
