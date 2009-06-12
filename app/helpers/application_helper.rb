2054d2b81b7b83ca8cf9b067b4b953faebf9e358
John W. Long <me@johnwlong.com>
2009-06-11 22:47:35 -0400

    pulled basic layout in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -149,6 +149,59 @@ module ApplicationHelper
     options_for_select([['<none>', '']] + TextFilter.descendants.map { |s| s.filter_name }.sort, selected)
   end
   
+  def body_classes
+    @body_classes ||= []
+  end
+  
+  # The NavTab Class holds the structure of a navigation tab (including
+  # its sub-nav items).
+  class NavTab < Array
+    attr_reader :name, :proper_name
+    
+    def initialize(name, proper_name)
+      @name, @proper_name = name, proper_name
+    end
+    
+    def [](id)
+      unless id.kind_of? Fixnum
+        self.find {|subnav_item| subnav_item.name.to_s == id.to_s }
+      else
+        super
+      end
+    end
+  end
+  
+  # Simple structure for storing the properties of a tab's sub items.
+  class NavSubItem
+    attr_reader :name, :proper_name, :url
+    
+    def initialize(name, proper_name, url = "#")
+      @name, @proper_name, @url = name, proper_name, url
+    end
+  end
+  
+  def nav_tabs
+    content = NavTab.new(:content, "Content")
+    content << NavSubItem.new(:pages, "Pages", admin_pages_path)
+    content << NavSubItem.new(:snippets, "Snippets", admin_snippets_path)
+    
+    design = NavTab.new(:design, "Design")
+    design << NavSubItem.new(:layouts, "Layouts", admin_layouts_path)
+    design << NavSubItem.new(:stylesheets, "Stylesheets", "#")
+    design << NavSubItem.new(:javascripts, "Javascripts", "#")
+    
+    # media = NavTab.new(:assets, "Assets")
+    # media << NavSubItem.new(:all, "All", "/admin/assets/")
+    # media << NavSubItem.new(:all, "Unattached", "/admin/assets/unattached/")
+    
+    settings = NavTab.new(:settings, "Settings")
+    settings << NavSubItem.new(:general, "Personal", edit_admin_preferences_path)
+    settings << NavSubItem.new(:users, "Users", admin_users_path)
+    settings << NavSubItem.new(:extensions, "Extensions", admin_extensions_path)
+    
+    [content, design, settings]
+  end
+  
   private
   
     def append_image_extension(name)
