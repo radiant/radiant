c4c2ca5c1bed49e0ed29ff933834decca831f646
Keith <keith@keithbingman.com>
2009-10-09 08:09:41 +0200

    updating vendor

--- vendor/extensions/textile_filter/lib/textile_tags.rb
+++ vendor/extensions/textile_filter/lib/textile_tags.rb
@@ -2,19 +2,21 @@ module TextileTags
   include Radiant::Taggable
 
   desc %{
-  #{I18n.t('tag_desc.textile.desc')}
+    Filters its contents with the Textile filter.
 
-    *#{I18n.t('tag_desc.usage')}:*
+    *Usage*:
 
     <pre><code><r:textile>
-    *&nbsp;First
-    *&nbsp;Second<br>&lt;/r:textile&gt;</code></pre>
+    * First
+    * Second
+    </r:textile></code></pre>
 
-    #{I18n.t('tag_desc.produces')}:
+    produces:
 
     <pre><code><ul>
-   &lt;li&gt;First&lt;/li&gt;
-   &lt;li&gt;Second&lt;/li&gt;<br>&lt;/ul&gt;</code></pre>
+      <li>First</li>
+      <li>Second</li>
+    </ul></code></pre>
   }
   tag 'textile' do |tag|
     TextileFilter.filter(tag.expand)
