72774214bd2007347de8082b8e1ad58d06e6102e
Jim Gay <jim@saturnflyer.com>
2009-12-29 23:28:33 -0500

    make 'reversed' class the standard for new and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_user
   before_filter :set_timezone
   before_filter :set_javascripts_and_stylesheets
+  before_filter :set_standard_body_style, :only => [:new, :edit]
   
   attr_accessor :config, :cache
   
@@ -70,5 +71,10 @@ class ApplicationController < ActionController::Base
       @stylesheets ||= []
       @stylesheets.concat %w(admin/main)
       @javascripts ||= []
+    end
+    
+    def set_standard_body_style
+      @body_classes ||= []
+      @body_classes.concat(%w(reversed))
     end
 end
