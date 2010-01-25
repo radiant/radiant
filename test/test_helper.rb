34e626868b36cf7b5a22adc935ee7a93710835ad
Jim Gay <jim@saturnflyer.com>
2010-01-25 18:42:12 -0500

    use ActiveSupport::TestCase. Closes #84

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,7 +14,7 @@ unless defined? TEST_ROOT
   end
   require 'test_help'
   
-  class Test::Unit::TestCase
+  class ActiveSupport::TestCase
     # Transactional fixtures accelerate your tests by wrapping each test method
     # in a transaction that's rolled back on completion.  This ensures that the
     # test database remains unchanged so your fixtures don't have to be reloaded
