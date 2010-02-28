dd14a56e62b5aff7a8f42bfe242fb994b8faa728
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-02-28 00:34:15 -0200

    remove vendored rails

--- vendor/rails/activerecord/test/cases/encoding_test.rb
+++
@@ -1,6 +0,0 @@
-require "cases/helper"
-
-class EncodingTest < ActiveRecord::TestCase
-  def test_uses_default_external_encoding
-  end
-end
--- vendor/rails/activerecord/test/models/event_author.rb
+++
@@ -1,5 +0,0 @@
-class EventAuthor < ActiveRecord::Base
-  belongs_to :author
-  belongs_to :event
-end
-
