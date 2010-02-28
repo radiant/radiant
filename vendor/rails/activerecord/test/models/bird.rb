b2496d6819e33e86dc755af1a1369a7dc157239a
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-02-28 00:34:15 -0200

    remove vendored rails

--- vendor/rails/activerecord/test/models/bird.rb
+++
@@ -1,9 +0,0 @@
-class Bird < ActiveRecord::Base
-  validates_presence_of :name
-
-  attr_accessor :cancel_save_from_callback
-  before_save :cancel_save_callback_method, :if => :cancel_save_from_callback
-  def cancel_save_callback_method
-    false
-  end
-end
