286690d553cd4d2166df99931912f221be4616b8
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-03-01 22:57:56 -0200

    move custom string and symbol extensions to "core_ext" initializer

--- config/initializers/core_ext.rb
+++ config/initializers/core_ext.rb
@@ -4,11 +4,7 @@ class String
   def symbolize
     self.gsub(/[^A-Za-z0-9]+/, "_").gsub(/(^_+|_+$)/, "").underscore.to_sym
   end
-  
-  def titlecase
-    self.gsub(/((?:^|\s)[a-z])/) { $1.upcase }
-  end
-  
+
   def to_name(last_part = '')
     self.underscore.gsub('/', ' ').humanize.titlecase.gsub(/\s*#{last_part}$/, '')
   end
@@ -22,4 +18,10 @@ class String
   alias :to_slug   :parameterize
   alias :slugify   :parameterize
   alias :slugerize :parameterize
+end
+
+class Symbol
+  def symbolize
+    self.to_s.symbolize
+  end
 end
