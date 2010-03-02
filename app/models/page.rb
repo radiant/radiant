20707b36053a1fa72f4d0022488a44abb82a11a6
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-03-01 23:09:28 -0200

    make default page parts fall back to ["body"] if not set in database

--- app/models/page.rb
+++ app/models/page.rb
@@ -16,6 +16,8 @@ class Page < ActiveRecord::Base
   belongs_to :layout
   belongs_to :created_by, :class_name => 'User'
   belongs_to :updated_by, :class_name => 'User'
+  
+  DEFAULT_PAGE_PARTS = %w[body]
 
   # Validations
   validates_presence_of :title, :slug, :breadcrumb, :status_id
@@ -286,7 +288,8 @@ class Page < ActiveRecord::Base
     private
 
       def default_page_parts(config = Radiant::Config)
-        default_parts = config['defaults.page.parts'].to_s.strip.split(/\s*,\s*/)
+        default_parts = config['defaults.page.parts'].presence || DEFAULT_PAGE_PARTS
+        default_parts = default_parts.strip.split(/\s*,\s*/) if String === default_parts
         default_parts.map do |name|
           PagePart.new(:name => name, :filter_id => config['defaults.page.filter'])
         end
