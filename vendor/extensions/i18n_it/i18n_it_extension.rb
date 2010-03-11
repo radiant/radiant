8b77cf68614017cb7bd5b6dad113d883f07fadec
Keith <keith@keithbingman.com>
2010-03-11 08:19:34 -0600

    renamed language packs

--- vendor/extensions/i18n_it/i18n_it_extension.rb
+++
@@ -1,8 +0,0 @@
-class I18nItExtension < Radiant::Extension
-  version "1.0"
-  description "Italian language pack for Radiant CMS"
-  url "http://yourwebsite.com/it"
-    
-  def activate
-  end
-end
