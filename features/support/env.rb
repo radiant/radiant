86582dcc1774df974a63001c615383742406b017
Jim Gay <jim@saturnflyer.com>
2010-04-12 00:18:30 -0400

    run features from user_system

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@
 # files.
 
 ENV["RAILS_ENV"] ||= "cucumber"
+
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
--- vendor/extensions/user_system/features/support/env.rb
+++ vendor/extensions/user_system/features/support/env.rb
@@ -4,13 +4,11 @@ ENV["RAILS_ENV"] = "test"
 extension_env = File.expand_path(File.dirname(__FILE__) + '/../../../../../config/environment')
 require extension_env+'.rb'
 
+require 'cucumber/rails/world'
+
+Dir.glob(File.join(File.dirname(__FILE__) + '..','..','..','..','..','features','**','*.rb')).each { |support| require support}
 Dir.glob(File.join(RADIANT_ROOT, "features", "**", "*.rb")).each {|step| require step}
  
 Cucumber::Rails::World.class_eval do
-  include Dataset
-  datasets_directory "#{RADIANT_ROOT}/spec/datasets"
-  Dataset::Resolver.default = Dataset::DirectoryResolver.new("#{RADIANT_ROOT}/spec/datasets", File.dirname(__FILE__) + '/../../spec/datasets', File.dirname(__FILE__) + '/../datasets')
-  self.datasets_database_dump_path = "#{Rails.root}/tmp/dataset"
-  
   # dataset :login_system
 end
