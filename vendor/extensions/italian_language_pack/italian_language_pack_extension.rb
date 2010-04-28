3ef351894fb84bcf90af95f2c44aa80ead27f8e4
Jim Gay <jim@saturnflyer.com>
2010-04-28 09:41:09 -0400

    removing italian

--- vendor/extensions/italian_language_pack/italian_language_pack_extension.rb
+++
@@ -1,8 +0,0 @@
-class ItalianLanguagePackExtension < Radiant::Extension
-  version "1.0"
-  description "Provides an Italian translation for the Radiant admin interface"
-  url "http://yourwebsite.com/it"
-    
-  def activate
-  end
-end
