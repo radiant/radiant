5ef4e570caea6d1cb02a3df6a05ab7d25eb8c9aa
Jim Gay <jim@saturnflyer.com>
2010-04-30 15:19:58 -0400

    update cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -49,6 +49,10 @@ Cucumber::Rails::World.use_transactional_fixtures = true
 
 # How to clean your database when transactions are turned off. See
 # http://github.com/bmabey/database_cleaner for more info.
-require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
-
+if defined?(ActiveRecord::Base)
+  begin
+    require 'database_cleaner'
+    DatabaseCleaner.strategy = :truncation
+  rescue LoadError => ignore_if_database_cleaner_not_present
+  end
+end
