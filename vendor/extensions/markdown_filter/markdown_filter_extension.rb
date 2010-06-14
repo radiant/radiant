42eaffb55c939b2b03d6e5b12e7a9f4ebdbc37b7
johnmuhl <git@johnmuhl.com>
2010-06-14 08:39:29 -0500

    Revert "don't try to load rubypants"
    This reverts commit a860f857c7050cbfb04f3520101396864e2d4784.

--- vendor/extensions/markdown_filter/markdown_filter_extension.rb
+++ vendor/extensions/markdown_filter/markdown_filter_extension.rb
@@ -6,6 +6,14 @@ rescue LoadError
   require 'kramdown'
 end
 
+begin
+  require 'rubypants'
+rescue LoadError
+  # If rubypants gem is not available, use packaged version
+  $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/vendor/rubypants"
+  retry
+end
+
 class MarkdownFilterExtension < Radiant::Extension
   version "1.0"
   description "Allows you to compose page parts or snippets using the Markdown or SmartyPants text filters."
