6eba899df19a7b39fe5264edbb4e0a3575df8632
Jim Gay <jim@saturnflyer.com>
2010-06-15 09:31:15 -0400

    use quoted strings instead of symbols

--- db/migrate/20081203140407_add_indexes.rb
+++ db/migrate/20081203140407_add_indexes.rb
@@ -1,19 +1,19 @@
 class AddIndexes < ActiveRecord::Migration
   def self.up
-    add_index :pages,       :class_name,            :name => :pages_class_name
-    add_index :pages,       :parent_id,             :name => :pages_parent_id
-    add_index :pages,       %w{slug parent_id},     :name => :pages_child_slug
-    add_index :pages,       %w{virtual status_id},  :name => :pages_published
+    add_index :pages,       :class_name,            :name => 'pages_class_name'
+    add_index :pages,       :parent_id,             :name => 'pages_parent_id'
+    add_index :pages,       %w{slug parent_id},     :name => 'pages_child_slug'
+    add_index :pages,       %w{virtual status_id},  :name => 'pages_published'
     
-    add_index :page_parts,  %w{page_id name},       :name => :parts_by_page
+    add_index :page_parts,  %w{page_id name},       :name => 'parts_by_page'
   end
 
   def self.down
-    remove_index :page_parts, :name => :parts_by_page
+    remove_index :page_parts, :name => 'parts_by_page'
                               
-    remove_index :pages,      :name => :pages_published
-    remove_index :pages,      :name => :pages_child_slug
-    remove_index :pages,      :name => :pages_parent_id
-    remove_index :pages,      :name => :pages_class_name
+    remove_index :pages,      :name => 'pages_published'
+    remove_index :pages,      :name => 'pages_child_slug'
+    remove_index :pages,      :name => 'pages_parent_id'
+    remove_index :pages,      :name => 'pages_class_name'
   end
 end
