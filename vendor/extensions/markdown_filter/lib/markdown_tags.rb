9b108c1c2d656f59a279251af3f71910eede3faf
johnmuhl <git@johnmuhl.com>
2010-06-17 17:43:17 -0500

    remove smarty_pants filter

--- vendor/extensions/markdown_filter/lib/markdown_tags.rb
+++ vendor/extensions/markdown_filter/lib/markdown_tags.rb
@@ -15,19 +15,4 @@ module MarkdownTags
   tag 'markdown' do |tag|
     MarkdownFilter.filter(tag.expand)
   end
-
-  desc %{
-    Filters its contents with the SmartyPants filter.
-
-    *Usage:*
-
-    <pre><code><r:smarty_pants>"A revolutionary quotation."</r:smarty_pants></code></pre>
-
-    produces
-
-    <pre><code>&#8220;A revolutionary quotation.&#8221;</code></pre>
-  }
-  tag 'smarty_pants' do |tag|
-    SmartyPantsFilter.filter(tag.expand)
-  end
 end
