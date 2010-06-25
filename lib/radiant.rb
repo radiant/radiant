17082b29e9c510c923a5ec32901bf8f1e8b74098
Jim Gay <jim@saturnflyer.com>
2010-06-25 01:04:36 -0400

    bump for next version

--- lib/radiant.rb
+++ lib/radiant.rb
@@ -5,8 +5,8 @@ unless defined? Radiant::Version
     module Version
       Major = '0'
       Minor = '9'
-      Tiny  = '0'
-      Patch = nil # set to nil for normal release
+      Tiny  = '1'
+      Patch = 'a' # set to nil for normal release
 
       class << self
         def to_s
