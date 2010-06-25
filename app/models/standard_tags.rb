4dc0db3ac2fb2a2aa3b8ff8c3b07f13bf9f7b88d
Jim Gay <jim@saturnflyer.com>
2010-06-24 22:22:15 -0400

    pull in aggregation tags

--- app/models/standard_tags.rb
+++ app/models/standard_tags.rb
@@ -343,6 +343,149 @@ module StandardTags
     children = tag.locals.page.children.count(:conditions => children_find_options(tag)[:conditions])
     tag.expand unless children > 0
   end
+  
+    desc %{
+    Aggregates the children of multiple URLs using the @urls@ attribute.
+    Useful for combining many different sections/categories into a single
+    feed or listing.
+    
+    *Usage*:
+    
+    <pre><code><r:aggregate urls="/section1; /section2; /section3"> ... </r:aggregate></code></pre>
+  }
+  tag "aggregate" do |tag|
+    raise "`urls' attribute required" unless tag.attr["urls"]
+    urls = tag.attr["urls"].split(";").map(&:strip).reject(&:blank?).map { |u| clean_url u }
+    parent_ids = urls.map {|u| Page.find_by_url(u) }.map(&:id)
+    tag.locals.parent_ids = parent_ids
+    tag.expand
+  end
+  
+  desc %{
+    Sets the scope to the individual aggregated page allowing you to
+    iterate through each of the listed urls.
+    
+    *Usage*:
+    
+    <pre><code><r:aggregate:each urls="/section1; /section2; /section3"> ... </r:aggregate:each></code></pre>
+  }
+  tag "aggregate:each" do |tag|
+    aggregates = []
+    tag.locals.aggregated_pages = tag.locals.parent_ids.map {|p| Page.find(p)}
+    tag.locals.aggregated_pages.each do |aggregate_page|
+      tag.locals.page = aggregate_page
+      aggregates << tag.expand
+    end
+    aggregates
+  end
+  
+  tag "aggregate:each:children" do |tag|
+    tag.locals.children = tag.locals.page.children
+    tag.expand
+  end
+  
+  tag "aggregate:each:children:each" do |tag|
+    options = children_find_options(tag)
+    result = []
+    children = tag.locals.children
+    tag.locals.previous_headers = {}
+    children.find(:all, options).each do |item|
+      tag.locals.child = item
+      tag.locals.page = item
+      result << tag.expand
+    end
+    result
+  end
+  
+  tag "aggregate:children" do |tag|
+    tag.expand
+  end
+  
+  desc %{
+    Renders the total count of children of the aggregated pages.  Accepts the
+    same options as @<r:children:each />@.
+
+    *Usage*:
+    
+    <pre><code><r:aggregate urls="/section1; /section2; /section3">
+      <r:children:count />
+    </r:aggregate></code></pre>
+  }  
+  tag "aggregate:children:count" do |tag|
+    options = aggregate_children(tag)
+    if ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
+      options[:group] = Page.columns.map {|c| c.name}.join(', ')
+      Page.find(:all, options).size
+    else
+      Page.count(options)
+    end
+  end
+  desc %{
+    Renders the contained block for each child of the aggregated pages.  Accepts the
+    same options as the plain @<r:children:each />@.
+
+    *Usage*:
+    
+    <pre><code><r:aggregate urls="/section1; /section2; /section3">
+      <r:children:each>
+        ...
+      </r:children:each>
+    </r:aggregate></code></pre>
+  }
+  tag "aggregate:children:each" do |tag|
+    options = aggregate_children(tag)
+    children = Page.find(:all, options)
+    tag.locals.previous_headers = {}
+    returning String.new do |output|
+      children.each do |child|
+        tag.locals.page = child
+        tag.locals.child = child
+        output << tag.expand
+      end
+    end
+  end
+  
+  desc %{
+    Renders the first child of the aggregated pages.  Accepts the
+    same options as @<r:children:each />@.
+
+    *Usage*:
+    
+    <pre><code><r:aggregate urls="/section1; /section2; /section3">
+      <r:children:first>
+        ...
+      </r:children:first>
+    </r:aggregate></code></pre>
+  }
+  tag "aggregate:children:first" do |tag|
+    options = aggregate_children(tag)
+    children = Page.find(:all, options)
+    if first = children.first
+      tag.locals.page = first
+      tag.expand
+    end
+  end
+  
+  desc %{
+    Renders the last child of the aggregated pages.  Accepts the
+    same options as @<r:children:each />@.
+
+    *Usage*:
+    
+    <pre><code><r:aggregate urls="/section1; /section2; /section3">
+      <r:children:last>
+        ...
+      </r:children:last>
+    </r:aggregate></code></pre>
+  }
+  tag "aggregate:children:last" do |tag|
+    options = aggregate_children(tag)
+    children = Page.find(:all, options)
+    if last = children.last
+      tag.locals.page = last
+      tag.expand
+    end
+  end
 
   desc %{
     Renders one of the passed values based on a global cycle counter.  Use the @reset@
@@ -1078,6 +1221,16 @@ module StandardTags
       else
         options[:conditions] = ["virtual = ?", false]
       end
+      options
+    end
+      
+    def aggregate_children(tag)
+      options = children_find_options(tag)
+      parent_ids = tag.locals.parent_ids
+    
+      conditions = options[:conditions]
+      conditions.first << " AND parent_id IN (?)"
+      conditions << parent_ids
       options
     end
     
--- spec/models/standard_tags_spec.rb
+++ spec/models/standard_tags_spec.rb
@@ -493,6 +493,51 @@ describe "Standard Tags" do
     end
   end
 
+  describe "<r:aggregate>" do
+    it "should raise an error when given no 'urls' attribute" do
+      pages(:home).should render('<r:aggregate></r:aggregate>').with_error("`urls' attribute required")
+    end
+    it "should expand its contents with a given 'urls' attribute formatted as '/url1; /url2;'" do
+      pages(:home).should render('<r:aggregate urls="/parent/child; /first;">true</r:aggregate>').as('true')
+    end
+  end
+  
+  describe "<r:aggregate:children>" do
+    it "should expand its contents" do
+      pages(:home).should render('<r:aggregate urls="/parent/child; /first;"><r:children>true</r:children></r:aggregate>').as('true') 
+    end
+  end
+  
+  describe "<r:aggregate:children:count>" do
+    it "should display the number of aggregated children" do
+      pages(:home).should render('<r:aggregate urls="/news; /assorted"><r:children:count /></r:aggregate>').as('14')
+    end
+  end
+  
+  describe "<r:aggregate:children:each>" do
+    it "should loop through each child from the given urls" do
+      pages(:home).should render('<r:aggregate urls="/parent; /news"><r:children:each><r:title/> </r:children:each></r:aggregate>').as('Article Article 2 Article 3 Article 4 Child Child 2 Child 3 ')
+    end
+    it "should sort the children by the given 'by' attribute" do
+      pages(:home).should render('<r:aggregate urls="/assorted; /news"><r:children:each by="slug"><r:slug /> </r:children:each></r:aggregate>').as('a article article-2 article-3 article-4 b c d e f g h i j ')
+    end
+    it "should order the children by the given 'order' attribute when used with 'by'" do
+      pages(:home).should render('<r:aggregate urls="/assorted; /news"><r:children:each by="slug" order="desc"><r:slug /> </r:children:each></r:aggregate>').as('j i h g f e d c b article-4 article-3 article-2 article a ')
+    end
+    it "should limit the number of results with the given 'limit' attribute" do
+      pages(:home).should render('<r:aggregate urls="/assorted; /news"><r:children:each by="slug" order="desc" limit="3"><r:slug /> </r:children:each></r:aggregate>').as('j i h ')
+    end
+  end
+  
+  describe "<r:aggregate:each>" do
+    it "should loop through each of the given aggregate urls" do
+      pages(:home).should render('<r:aggregate urls="/parent/child; /first; /assorted;"><r:each><r:title /> </r:each></r:aggregate>').as('Child First Assorted ')
+    end
+    it "should display it's contents in the scope of the individually aggregated page" do
+      pages(:home).should render('<r:aggregate urls="/parent; /news; /assorted;"><r:each><r:children:each><r:title /> </r:children:each></r:each></r:aggregate>').as('Child Child 2 Child 3 Article Article 2 Article 3 Article 4 a b c d e f g h i j ')
+    end
+  end
+
   describe "<r:author>" do
     it "should render the author of the current page" do
       page.should render('<r:author />').as('Admin')
