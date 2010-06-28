31778559734d01b7eb461ea2eb037f3b1ec392be
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-06-28 05:54:01 +0200

    don't use ExtensionLoader while looking up available locales for now

--- lib/radiant/available_locales.rb
+++ lib/radiant/available_locales.rb
@@ -1,12 +1,14 @@
+# require_dependency 'radiant/extension_loader'
+
 module Radiant::AvailableLocales
   
   def self.locale_paths
-    root_locales = [File.join(RADIANT_ROOT, 'config', 'locales'), File.join(RAILS_ROOT, 'config', 'locales')].uniq
-    unless root_locales.empty?
-      Radiant::ExtensionLoader.locale_paths.dup + root_locales
-    else
-      Radiant::ExtensionLoader.locale_paths
-    end
+    root_locales = [ Rails.root + 'config/locales' ]
+    # unless root_locales.empty?
+    #   Radiant::ExtensionLoader.locale_paths.dup + root_locales
+    # else
+    #   Radiant::ExtensionLoader.locale_paths
+    # end
   end
     
   def self.locales
@@ -19,7 +21,7 @@ module Radiant::AvailableLocales
           # filters out the available_tags files
           result =~ /\_available_tags/ ? nil : result
         end.compact.each do |str|
-          locale_file = YAML.load_file(path + "/" + str + ".yml")
+          locale_file = YAML.load_file(path + "#{str}.yml")
           lang = locale_file[str]["this_file_language"] if locale_file[str]
           available_locales.merge! Hash[lang, str] if lang
         end.freeze
