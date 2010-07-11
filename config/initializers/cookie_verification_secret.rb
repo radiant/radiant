6c3ec3bda65d2815fe483aafb84c01fef527942a
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-11 13:36:43 +0200

    remove unnecessary "cookie_verification_secret" initializer

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '9b1617c9a53ea25cf5863950f514fb07f9e38ca56c0890e5c53fb3044499b3c17cafc482ce8a51b5fa335ce85978f275185709c7149b787c5cc8c8ae597c4d12'
