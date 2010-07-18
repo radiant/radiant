828cee4c2cef632ba587eb04c026c7123a123c6b
Josh French <josh@digitalpulp.com>
2010-07-18 19:42:04 -0400

    Move page subclass detection into a separate method so extensions can hook it

--- app/controllers/admin/page_types_controller.rb
+++ app/controllers/admin/page_types_controller.rb
@@ -1,7 +1,13 @@
 class Admin::PageTypesController < ApplicationController
   def index
     @page = Page.find(params[:page_id])
-    @options = Page.descendants.sort_by(&:name)
+    @options = page_types
     render :layout => false
   end
+
+  private
+
+    def page_types
+      Page.descendants.sort_by(&:name)
+    end
 end
