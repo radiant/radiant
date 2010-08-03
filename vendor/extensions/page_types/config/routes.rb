e97688094258d75b45416e3c5ab9316c3e9f2d0d
Josh French <josh@digitalpulp.com>
2010-08-03 11:18:26 -0400

    Remove page types controller

--- vendor/extensions/page_types/config/routes.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.namespace :admin do |admin|
-    admin.page_types '/pages/:page_id/types', :controller => 'page_types', :action => 'index', :conditions => { :method => :get }
-  end
-end
