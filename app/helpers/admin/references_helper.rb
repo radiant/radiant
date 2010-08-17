1cacb93ba78e97034103ce62efa9f00d6c3d0d39
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-08-17 12:19:48 +0200

    `Object.returning` → tap

--- app/helpers/admin/references_helper.rb
+++ app/helpers/admin/references_helper.rb
@@ -1,6 +1,6 @@
 module Admin::ReferencesHelper
   def tag_reference
-    returning String.new do |output|
+    String.new.tap do |output|
       class_of_page.tag_descriptions.sort.each do |tag_name, description|
         value = t("desc.#{tag_name.gsub(':','-')}").match('desc') ? description : t("desc.#{tag_name.gsub(':','-')}")
         output << render(:partial => "admin/references/tag_reference.haml", 
