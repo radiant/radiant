61436153a036fb0680f609d60ad5b92c85024525
spanner <will@spanner.org>
2010-09-22 09:58:14 +0100

    config/routes.rb in test extension

--- test/fixtures/extensions/basic/basic_extension.rb
+++ test/fixtures/extensions/basic/basic_extension.rb
@@ -2,12 +2,7 @@ class BasicExtension < Radiant::Extension
   version "1.1"
   description "just a test"
   url "http://test.com"
-  
-  define_routes do |map|
-    map.connect '/your/routing', :controller => 'basic_extension', :action => 'routing'
-    # map.connect ':controller/:action'
-  end
-  
+    
   def activate
     tab 'Content' do
       add_item("Basic Extension Tab", "/admin/basic", :before => 'Pages')
