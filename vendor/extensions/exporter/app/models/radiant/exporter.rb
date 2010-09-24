f1d982456d2bd9ced4d485408d13d274019f8346
Jim Gay <jim@saturnflyer.com>
2010-09-24 17:58:58 -0400

    updated specs, added exportable_models cattr_accessor

--- vendor/extensions/exporter/app/models/radiant/exporter.rb
+++ vendor/extensions/exporter/app/models/radiant/exporter.rb
@@ -1,8 +1,11 @@
 module Radiant
   class Exporter
+    cattr_accessor :exportable_models
+    @@exportable_models = [Radiant::Config, User, Page, PagePart, PageField, Snippet, Layout]
+    
     def self.export
       hash = {}
-      [Radiant::Config, User, Page, PagePart, PageField, Snippet, Layout].each do |klass|
+      @@exportable_models.each do |klass|
         hash[klass.name.pluralize] = klass.find(:all).inject({}) { |h, record| h[record.id.to_i] = record.attributes; h }
       end
       hash.to_yaml
--- vendor/extensions/exporter/spec/models/radiant/exporter_spec.rb
+++ vendor/extensions/exporter/spec/models/radiant/exporter_spec.rb
@@ -2,24 +2,26 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe Radiant::Exporter do
   dataset :pages_with_layouts, :users_and_pages, :snippets
-
-  before :each do
-    @exporter = Radiant::Exporter
-    @output = @exporter.export
-    @hash = YAML::load(@output)
-    @classes  = ['Radiant::Configs', 'Users', 'Pages', 'PageParts', 'Snippets', 'Layouts', 'PageFields']
-  end
   
-  it "should output a string" do
-    @output.should be_kind_of(String)
-  end
+  let(:exporter){ Radiant::Exporter }
+  let(:exported_content){ exporter.export }
+  let(:exported_hash){ YAML::load(exported_content) }
+  subject { exporter }
   
-  it "should output all Radiant models" do
-    @classes.all? { |c| @hash.has_key?(c) }.should be_true
+  specify{ exported_content.should be_kind_of(String) }
+  
+  it "should output all exportable_models with pluralized class names as keys" do
+    exporter.exportable_models.all? { |c| exported_hash.has_key?(c.to_s.pluralize) }.should be_true
   end
   
   it "should output the models by id as hashes" do
-    @hash['Pages'][page_id(:home)]['title'].should == pages(:home).title
-    @hash['Users'][user_id(:admin)]['name'].should == users(:admin).name
+    exported_hash['Pages'][page_id(:home)]['title'].should == pages(:home).title
+    exported_hash['Users'][user_id(:admin)]['name'].should == users(:admin).name
+  end
+  
+  its(:exportable_models){ should == [Radiant::Config, User, Page, PagePart, PageField, Snippet, Layout] }
+  it "should allow setting exportable_models" do
+    exporter.exportable_models = [Page]
+    exporter.exportable_models.should == [Page]
   end
 end
