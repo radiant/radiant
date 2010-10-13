a39c5990d54026091fcdf67c05f5d56bd13f161e
spanner <will@spanner.org>
2010-10-13 17:15:35 +0100

    documentation brought up to date

--- app/models/radiant/config.rb
+++ app/models/radiant/config.rb
@@ -13,7 +13,7 @@ module Radiant
   class Config < ActiveRecord::Base
     #
     # The Radiant.config model class is stored in the database (and cached) but emulates a hash 
-    # with simple bracket methods which allow you to get and set values like so:
+    # with simple bracket methods that allow you to get and set values like so:
     #
     #   Radiant.config['setting.name'] = 'value'
     #   Radiant.config['setting.name'] #=> "value"
@@ -24,7 +24,7 @@ module Radiant
     # declared by calling Radiant::Config#define:
     # 
     #   # setting must be either 'foo', 'bar' or 'blank'
-    #   define('admin.name', :select_from => ['foo', 'bar'], :label => "Name of administrator")
+    #   define('admin.name', :select_from => ['foo', 'bar'])
     #
     #   # setting is (and must be) chosen from the names of currently available layouts
     #   define('shop.layout', :select_from => lambda { Layout.all.map{|l| [l.name,l.id]} }, :alow_blank => false)
@@ -36,7 +36,7 @@ module Radiant
     #
     #   Radiant.config do |config|
     #     config.namespace('user', :allow_change => true) do |user|
-    #       user.define 'allow_password_reset?', :label => 'Allow password reset?', :default => true
+    #       user.define 'allow_password_reset?', :default => true
     #     end
     #   end
     #
@@ -59,7 +59,7 @@ module Radiant
     # Helper methods are defined in ConfigurationHelper that will display config entry values
     # or edit fields:
     # 
-    #   # to display label and value, where label is either the defined :label or the config key:
+    #   # to display label and value, where label comes from looking up the config key in the active locale
     #   show_setting('admin.name') 
     #
     #   # to display an appropriate checkbox, text field or select box with label:
@@ -144,8 +144,6 @@ module Radiant
       #
       # Can take several options:
       # * :default is the value that will be placed in the database if none has been set already
-      # * :label is the title used to display this setting in the admin interface. Defaults to the key if not supplied.
-      # * :notes is a help message that can be displayed in the admin interface to explain the use of this config item.
       # * :type can be :string, :boolean or :integer. Note that all settings whose key ends in ? are considered boolean.
       # * :select_from should be a list or hash suitable for passing to options_for_select, or a block that will return such a list at runtime
       # * :validate_with should be a block that will receive a value and return true or false. Validations are also implied by type or select_from.
@@ -153,16 +151,12 @@ module Radiant
       # * :allow_change should be false if the config item can only be set, not changed. Add a default to specify an unchanging config entry.
       # * :allow_display should be false if the config item should not be showable in radius tags
       #
-      # From the main radiant config/settings.rb:
+      # From the main radiant config/initializers/radiant_config.rb:
       #
-      #   config.namespace('defaults', :allow_change => true, :allow_blank => true) do |defaults|
-      #     defaults.define 'locale', :label => 'Default language', :select_from => lambda { Radiant::AvailableLocales.locales }
-      #     defaults.namespace('page') do |page|
-      #       page.define 'parts', :label => 'Default page parts', :notes => 'comma separated list of part names', :default => "Body,Extended"
-      #       page.define 'status', :select_from => lambda { Status.selectable_values }, :label => "Default page status", :default => "Draft"
-      #       page.define 'filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :label => "Default text filter"
-      #       page.define 'fields', :label => 'Default page fields', :notes => 'comma separated list of field names'
-      #     end
+      #   Radiant.config do |config|
+      #     config.define 'defaults.locale', :select_from => lambda { Radiant::AvailableLocales.locales }, :allow_blank => true
+      #     config.define 'defaults.page.parts', :default => "Body,Extended"
+      #     ...
       #   end
       #
       # It's also possible to reuse a definition by passing it to define:
@@ -171,7 +165,7 @@ module Radiant
       #   define "my.layout", choose_layout
       #   define "your.layout", choose_layout
       #
-      # but that's only used in testing at the moment.
+      # but at the moment that's only done in testing.
       #
       def define(key, options={})
         if options.is_a? Radiant::Config::Definition
@@ -257,12 +251,6 @@ module Radiant
     #
     def definition
       @definition ||= self.class.definition_for(self.key)
-    end
-
-    # Returns the label for this item as defined in its definition, or the key if no label (or no definition) has been defined.
-    #
-    def label
-      definition.label || self.key
     end
 
     # Returns true if the item key ends with '?' or the definition specifies :type => :boolean.
--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -3,7 +3,7 @@ Radiant.config do |config|
   config.define 'dev.host'
   config.define 'local.timezone', :allow_change => true, :select_from => lambda { ActiveSupport::TimeZone::MAPPING.keys.sort }
   config.define 'defaults.locale', :select_from => lambda { Radiant::AvailableLocales.locales }, :allow_blank => true
-  config.define 'defaults.page.parts', :notes => 'comma separated list of part names', :default => "Body,Extended"
+  config.define 'defaults.page.parts', :default => "Body,Extended"
   config.define 'defaults.page.status', :select_from => lambda { Status.selectable_values }, :allow_blank => true, :default => "Draft"
   config.define 'defaults.page.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
   config.define 'defaults.page.fields'
