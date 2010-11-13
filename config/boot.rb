e7f48d2a056a2c62c4bfbd237fe98349cb80b3ae
Dirk Kelly <dk@dirkkelly.com>
2010-11-13 09:26:11 +0800

    Commented out boot code, probably will be deleting this

--- config/boot.rb
+++ config/boot.rb
@@ -1,26 +1,29 @@
-require 'rubygems'
-
-# Set up gems listed in the Gemfile.
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-begin
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
-  require 'rails'
-rescue Bundler::GemNotFound => e
-  STDERR.puts e.message
-  STDERR.puts "Try running `bundle install`."
-  exit!
-end if File.exist?(gemfile)
-
-module Radiant
-  def self.loaded_via_gem?
-    false
-  end
-
-  def self.app?
-    true
-  end
-
-  Version = '0.9.rails3'
-end
+# Rails 3 Engine - Part of ../radiant.rb
+#
+#
+# require 'rubygems'
+#
+# # Set up gems listed in the Gemfile.
+# gemfile = File.expand_path('../../Gemfile', __FILE__)
+# begin
+#   ENV['BUNDLE_GEMFILE'] = gemfile
+#   require 'bundler'
+#   Bundler.setup
+#   require 'rails'
+# rescue Bundler::GemNotFound => e
+#   STDERR.puts e.message
+#   STDERR.puts "Try running `bundle install`."
+#   exit!
+# end if File.exist?(gemfile)
+#
+# module Radiant
+#   def self.loaded_via_gem?
+#     false
+#   end
+#
+#   def self.app?
+#     true
+#   end
+#
+#   Version = '0.9.rails3'
+# end
