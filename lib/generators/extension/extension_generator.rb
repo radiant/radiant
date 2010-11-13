14de2fcf9e2b5f7bb55189a7f34a8dc6588ebd4f
Dirk Kelly <dk@dirkkelly.com>
2010-11-13 15:46:15 +0800

    moved extension generator into rails 3 space

--- lib/generators/extension/extension_generator.rb
+++ lib/generators/extension/extension_generator.rb
@@ -1,11 +1,7 @@
-begin
-  require 'git'
-rescue LoadError
-end
-
 class ExtensionGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   argument :extension_name, :type => :string, :default => 'site'
+  class_option :with_test_unit, :type => :boolean, :default => false
   
   def directories
     empty_directory "#{extension_path}/app/controllers"
@@ -44,13 +40,44 @@ class ExtensionGenerator < Rails::Generators::Base
     end
   end
   
+  def rspec
+    unless options.with_test_unit?
+      empty_directory "#{extension_path}/spec/controllers"
+      empty_directory "#{extension_path}/spec/models"
+      empty_directory "#{extension_path}/spec/views"
+      empty_directory "#{extension_path}/spec/helpers"
+      empty_directory "#{extension_path}/features/support"
+      empty_directory "#{extension_path}/features/step_definitions/admin"
+
+      template  'rspec/Rakefile',          "#{extension_path}/Rakefile"
+      template  'rspec/spec_helper.rb',    "#{extension_path}/spec/spec_helper.rb"
+      copy_file 'rspec/_.rspec',           "#{extension_path}/spec/.rspec"
+      copy_file 'rspec/cucumber.yml',      "#{extension_path}/cucumber.yml"
+      template  'rspec/cucumber_env.rb',   "#{extension_path}/features/support/env.rb"
+      template  'rspec/cucumber_paths.rb', "#{extension_path}/features/support/paths.rb"
+    end
+  end
+
+  def testunit
+    if options.with_test_unit?
+      empty_directory "#{extension_path}/test/fixtures"
+      empty_directory "#{extension_path}/test/functional"
+      empty_directory "#{extension_path}/test/unit"
+
+      template 'test/Rakefile',            "#{extension_path}/Rakefile"
+      template 'test/test_helper.rb',      "#{extension_path}/test/test_helper.rb"
+      template 'test/functional_test.rb',  "#{extension_path}/test/functional/#{extension_file_name}_test.rb"
+    end
+  end
+
+
   private
 
   def class_name
     "#{extension_name.classify}Extension"
   end
   
-  def extension_file_name
+  def file_name
     "#{extension_name.underscore}_extension"
   end
 
@@ -59,7 +86,27 @@ class ExtensionGenerator < Rails::Generators::Base
   end
 
   def homepage
-    %x[git config user.name] ? "http://github.com/#{%x[git config user.name].gsub("\n",'')}/radiant-#{extension_file_name}-extension" : "http://yourwebsite.com/#{extension_file_name}"
+    author_name ? "http://github.com/#{author_name}/radiant-#{file_name}-extension" : "http://yourwebsite.com/#{file_name}"
   end
   
+  def author_info
+    @author_info ||= begin
+      Git.global_config
+    rescue NameError
+      {}
+    end
+  end
+
+  def homepage
+    author_info['github.user'] ? "http://github.com/#{author_info['github.user']}/radiant-#{file_name}-extension" : "http://yourwebsite.com/#{file_name}"
+  end
+
+  def author_email
+    author_info['user.email'] || 'your email'
+  end
+
+  def author_name
+    author_info['user.name'] || 'Your Name'
+  end
+
 end
