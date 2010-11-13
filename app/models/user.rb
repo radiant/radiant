e413153942a0cf8e015d787b4c1e7d2116825745
Dirk Kelly <dk@dirkkelly.com>
2010-11-13 13:05:53 +0800

    missing auth code added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ActiveRecord::Base
   validates_length_of :password, :within => 5..40, :allow_nil => true, :if => :validate_length_of_password?
   validates_length_of :email, :maximum => 255, :allow_nil => true
 
+  validates_numericality_of :id, :only_integer => true, :allow_nil => true
+
   attr_writer :confirm_password
   class << self
     def unprotected_attributes
@@ -40,10 +42,57 @@ class User < ActiveRecord::Base
     respond_to?("#{role}?") && send("#{role}?")
   end
 
+  def sha1(phrase)
+    Digest::SHA1.hexdigest("--#{salt}--#{phrase}--")
+  end
+
+  def self.authenticate(login_or_email, password)
+    user = find(:first, :conditions => ["login = ? OR email = ?", login_or_email, login_or_email])
+    user if user && user.authenticated?(password)
+  end
+
+  def authenticated?(password)
+    self.password == sha1(password)
+  end
+
+  def after_initialize
+    @confirm_password = true
+  end
+
+  def confirm_password?
+    @confirm_password
+  end
+
+  def remember_me
+    update_attribute(:session_token, sha1(Time.now + Radiant::Config['session_timeout'].to_i)) unless self.session_token?
+  end
+
+  def forget_me
+    update_attribute(:session_token, nil)
+  end
+
   private
 
     def validate_length_of_password?
       new_record? or not password.to_s.empty?
+    end
+
+    before_create :encrypt_password
+    def encrypt_password
+      self.salt = Digest::SHA1.hexdigest("--#{Time.now}--#{login}--sweet harmonious biscuits--")
+      self.password = sha1(password)
+    end
+
+    before_update :encrypt_password_unless_empty_or_unchanged
+    def encrypt_password_unless_empty_or_unchanged
+      user = self.class.find(self.id)
+      case password
+      when ''
+        self.password = user.password
+      when user.password
+      else
+        encrypt_password
+      end
     end
 
 end
