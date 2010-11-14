df26246eebeb7b9375ed05831cb848b815693bb5
Dirk Kelly <dk@dirkkelly.com>
2010-11-14 12:25:50 +0800

    Removed the version module from the engine

--- lib/radiant/engine.rb
+++ lib/radiant/engine.rb
@@ -98,26 +98,4 @@ module Radiant
     end
   end
   
-  module Version
-    Major = '0'
-    Minor = '9'
-    Tiny  = '1'
-    Patch = nil # set to nil for normal release
-
-    class << self
-      def to_s
-        [Major, Minor, Tiny, Patch].delete_if{|v| v.nil? }.join('.')
-      end
-      alias :to_str :to_s
-    end
-  end
-  
-  def self.loaded_via_gem?
-    false
-  end
-  
-  def self.app?
-    true
-  end
-  
 end
