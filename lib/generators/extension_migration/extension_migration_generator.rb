0c9ae35eb58ad16f48a68a5e4f2e0d326c031d44
Dirk Kelly <dk@dirkkelly.com>
2010-11-14 09:30:28 +0800

    Extensioin Migration Generator. Hooks the standard active record

---
+++ lib/generators/extension_migration/extension_migration_generator.rb
@@ -0,0 +1,23 @@
+begin
+  require 'rails/generators/active_record/migration/migration_generator'
+rescue LoadError
+end
+
+ActiveRecord::Generators::MigrationGenerator.class_eval do
+  remove_argument :name
+  remove_argument :attributes
+
+  argument :extension_name, :as => :string
+  argument :name, :as => :string
+  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+
+  def create_migration_file
+    set_local_assigns!
+    migration_template "migration.rb", "vendor/extensions/#{extension_name}/db/migrate/#{file_name}.rb"
+  end
+end
+
+
+class ExtensionMigrationGenerator < Rails::Generators::Base
+  hook_for :orm, :required => true, :as => :migration
+end
