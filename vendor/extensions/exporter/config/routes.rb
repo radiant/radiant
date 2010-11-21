31af6149a698a2976c74d299446279b94646ed3a
Dirk Kelly <dk@dirkkelly.com>
2010-11-21 16:47:49 +0800

    Removing extensions which will be accessed as gems

--- vendor/extensions/exporter/config/routes.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.with_options(:controller => 'admin/export') do |export|
-    export.export          'admin/export',                             :action => 'yaml'
-    export.export_yaml     'admin/export/yaml',                        :action => 'yaml'
-  end
-end
