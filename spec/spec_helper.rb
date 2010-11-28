65a9322f9663ec8140d4f9b172d1e64681733d53
Dirk Kelly <dk@dirkkelly.com>
2010-11-28 14:10:18 +0800

    spec_helper will now load test helpers and datasets

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,30 +3,22 @@ unless defined? SPEC_ROOT
 
   SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
 
-  require File.expand_path("../../config/environment", __FILE__)
+  require File.expand_path(File.join(SPEC_ROOT, '../../../', 'config/environment'))
   require 'rspec/rails'
   require 'dataset'
   require 'dataset/extensions/rspec'
 
-  Dir["#{Rails.root}/spec/support/**/*.rb"].each {|f| require f}
+  Dataset::Resolver.default = Dataset::DirectoryResolver.new("#{SPEC_ROOT}/datasets")
+
+  Dir["#{SPEC_ROOT}/support/**/*.rb"].each {|f| require f}
 
   RSpec.configure do |config|
     include Spec::Rails::Matchers
 
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
     config.mock_with :rspec
 
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, comment the following line or assign false
-    # instead of true.
     config.use_transactional_fixtures = true
     config.use_instantiated_fixtures  = false
   end
