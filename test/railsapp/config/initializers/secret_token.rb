1390c5503c2818e06f8f12f412a1e3d18237e7c2
Dirk Kelly <dk@dirkkelly.com>
2010-12-25 12:11:09 +0800

    This is our test rails app, this is where we are going to ensure that radiant is working. It doesn't get built into the gem.

---
+++ test/railsapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsapp::Application.config.secret_token = 'd5544535f12cbf9ae1b09b5f878dc0687fb29d6e2807f97beb7417cff79d3870cf9b2c3fa7794244aac4ae93ed9d64f3820581f6e1c871a414491dab9489f22c'
