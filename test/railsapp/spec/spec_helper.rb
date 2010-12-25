137c78634738a54c46c7075300459594f9a86473
Dirk Kelly <dk@dirkkelly.com>
2010-12-25 13:12:05 +0800

    you can now run rake spec (not that you want to see those 500~ failing specs)

--- test/railsapp/spec/spec_helper.rb
+++ test/railsapp/spec/spec_helper.rb
@@ -1,25 +1,33 @@
-unless defined? SPEC_ROOT
-  ENV["RAILS_ENV"] = "test"
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
 
-  SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
+require File.expand_path(File.join(SPEC_ROOT, '..', 'config/environment'))
+require 'rspec/rails'
 
-  require File.expand_path(File.join(SPEC_ROOT, '..', 'config/environment'))
-  require 'rspec/rails'
-  require 'dataset'
-  require 'dataset/extensions/rspec'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  Dataset::Resolver.default = Dataset::DirectoryResolver.new("#{SPEC_ROOT}/datasets")
+require 'dataset'
+require 'dataset/extensions/rspec'
+Dataset::Resolver.default = Dataset::DirectoryResolver.new("#{SPEC_ROOT}/datasets")
 
-  Dir["#{SPEC_ROOT}/support/**/*.rb"].each {|f| require f}
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
 
-  RSpec.configure do |config|
-    include Spec::Rails::Matchers
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    config.mock_with :rspec
-
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-    config.use_transactional_fixtures = true
-    config.use_instantiated_fixtures  = false
-  end
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
 end
