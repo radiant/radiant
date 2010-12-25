be0976b105fd5e3af327545075897d4eb09b86d0
Dirk Kelly <dk@dirkkelly.com>
2010-12-25 12:11:29 +0800

    Removing a lot of files which are now part of the test/railsapp

--- config/application.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path('../boot', __FILE__)
-require 'rails/all'
-
-# Auto-require default libraries and those for the current Rails environment.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
