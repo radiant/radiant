ffa88cec37f9307513f7e413c2f79961852695d9
Dirk Kelly <dk@dirkkelly.com>
2010-12-28 08:34:26 +0800

    spec_helper update to load the moved support files

--- test/railsapp/spec/spec_helper.rb
+++ test/railsapp/spec/spec_helper.rb
@@ -7,7 +7,7 @@ require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.join(SPEC_ROOT, 'support/**/*.rb')].each {|f| require f}
+Dir[File.join(RADIANT_ROOT, 'spec/support/**/*.rb')].each {|f| require f}
 
 require 'dataset'
 require 'dataset/extensions/rspec'
