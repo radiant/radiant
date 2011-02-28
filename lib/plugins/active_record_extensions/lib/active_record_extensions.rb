b1220de1af1bf50911194e45530f2b21e9582791
spanner <will@spanner.org>
2011-02-28 13:09:47 +0000

    validate_path helper to check that a page exists at a specified path

--- lib/plugins/active_record_extensions/lib/active_record_extensions.rb
+++ lib/plugins/active_record_extensions/lib/active_record_extensions.rb
@@ -1,6 +1,15 @@
 require 'active_record'
 
 class ActiveRecord::Base
+  
+  def self.validates_path(*args)
+    configuration = args.extract_options!
+    validates_each(args, configuration) do |record, attr_name, value|
+      page = Page.find_by_path(value)
+      record.errors.add(attr_name, :page_not_found, :default => configuration[:message]) if page.nil? || page.is_a?(FileNotFoundPage)
+    end
+  end
+  
   def self.object_id_attr(symbol, klass)
     module_eval %{
       def #{symbol}
@@ -15,4 +24,5 @@ class ActiveRecord::Base
       end
     }
   end
+  
 end
