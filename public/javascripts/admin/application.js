c28eb805b138e7093d71ab02610a5b131016b885
spanner <will@spanner.org>
2011-03-02 16:48:32 +0000

    table.pages instead of table#pages

--- public/javascripts/admin/application.js
+++ public/javascripts/admin/application.js
@@ -35,7 +35,7 @@ Event.addBehavior({
   
   'a.popup': Popup.TriggerBehavior(),
   
-  'table#pages': SiteMapBehavior(),
+  'table.pages': SiteMapBehavior(),
   
   'input#page_title': function() {
     var title = this;
