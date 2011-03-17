05c256dfaafcce6aa8843cfadb49ee0c8cc3e672
Jim Gay <jim@saturnflyer.com>
2011-03-16 22:13:17 -0400

    bump major version

--- lib/radiant.rb
+++ lib/radiant.rb
@@ -3,10 +3,10 @@ RADIANT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) unless 
 unless defined? Radiant::Version
   module Radiant
     module Version
-      Major = '0'
-      Minor = '9'
-      Tiny  = '2'
-      Patch = 'a' # set to nil for normal release
+      Major = '1'
+      Minor = '0'
+      Tiny  = '0'
+      Patch = 'alpha' # set to nil for normal release
 
       class << self
         def to_s
