47891ffaf9e4cb39ab1001ba0d37ecae970de1ff
jsntv200 <jsntv200@gmail.com>
2011-03-27 17:17:28 +1100

    modify locales to use Dir.glob so that dot files are ignored

--- lib/radiant/available_locales.rb
+++ lib/radiant/available_locales.rb
@@ -1,5 +1,5 @@
 module Radiant::AvailableLocales
-  
+
   def self.locale_paths
     root_locales = [File.join(RADIANT_ROOT, 'config', 'locales'), File.join(RAILS_ROOT, 'config', 'locales')].uniq
     unless root_locales.empty?
@@ -8,18 +8,18 @@ module Radiant::AvailableLocales
       Radiant::ExtensionLoader.locale_paths
     end
   end
-    
+
   def self.locales
     available_locales = {}
-    
-    locale_paths.each do |path|    
+
+    locale_paths.each do |path|
       if File.exists? path
-        Dir.new(path).entries.collect do |x|
-          result = x =~ /\.yml/ ? x.sub(/\.yml/,"") : nil
+        Dir.glob(path + "/*.yml").collect do |x|
+          result = File.basename(x, ".yml")
           # filters out the available_tags files
           result =~ /\_available_tags/ ? nil : result
         end.compact.each do |str|
-          locale_file = YAML.load_file(path + "/" + str + ".yml")
+          locale_file = YAML.load_file(File.join(path, str) + ".yml")
           lang = locale_file[str]["this_file_language"] if locale_file[str]
           available_locales.merge! Hash[lang, str] if lang
         end.freeze
