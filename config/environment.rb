48fab6e3798df50e11ebfa9091f80b6537461605
Jeff Casimir <jeff@casimircreative.com>
2011-04-05 10:44:27 -0400

    Adds unexpressed HAML dependency. Also, 3.0.16 was causing an error, specifying 3.0.25 exactly

--- config/environment.rb
+++ config/environment.rb
@@ -89,4 +89,5 @@ Radiant::Initializer.run do |config|
   
   config.gem 'compass', :version => '~> 0.10.6'
   config.gem 'will_paginate', :version => '~> 2.3.11'
+  config.gem 'haml', :version => '3.0.25'
 end
