6b5c25b38d48db89adac0f837f17b19ec5261dd8
Jim Gay <jim@saturnflyer.com>
2011-05-17 14:30:37 -0400

    update instance boot

--- lib/generators/instance/templates/instance_boot.rb
+++ lib/generators/instance/templates/instance_boot.rb
@@ -1,7 +1,7 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
-RAILS_ROOT = File.expand_path("#{File.dirname(__FILE__)}/..") unless defined?(RAILS_ROOT)
+RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
 module Rails
   class << self
@@ -46,7 +46,7 @@ module Radiant
     def preinitialize
       load(preinitializer_path) if File.exist?(preinitializer_path)
     end
-    
+
     def loaded_via_gem?
       pick_boot.is_a? GemBoot
     end
@@ -82,6 +82,8 @@ module Radiant
         exit 1
       end
       Radiant::Initializer.run(:set_load_path)
+      Radiant::Initializer.run(:install_gem_spec_stubs)
+      Rails::GemDependency.add_frozen_gem_path
     end
   end
 
@@ -113,9 +115,9 @@ module Radiant
       load_radiant_gem
       super
     end
-      
+
     def load_error_message
-     "Please reinstall the Radiant gem with the command 'gem install radiant'."
+      "Please reinstall the Radiant gem with the command 'gem install radiant'."
     end
 
     def load_radiant_gem
@@ -125,8 +127,12 @@ module Radiant
         gem 'radiant'
       end
     rescue Gem::LoadError => load_error
-      $stderr.puts %(Missing the Radiant #{version} gem. Please `gem install -v=#{version} radiant`, update your RADIANT_GEM_VERSION setting in config/environment.rb for the Radiant version you do have installed, or comment out RADIANT_GEM_VERSION to use the latest version installed.)
-      exit 1
+      if load_error.message =~ /Could not find RubyGem radiant/
+        STDERR.puts %(Missing the Radiant #{version} gem. Please `gem install -v=#{version} radiant`, update your RADIANT_GEM_VERSION setting in config/environment.rb for the Radiant version you do have installed, or comment out RADIANT_GEM_VERSION to use the latest version installed.)
+        exit 1
+      else
+        raise
+      end
     end
 
     class << self
@@ -145,15 +151,15 @@ module Radiant
       end
 
       def load_rubygems
+        min_version = '1.3.2'
         require 'rubygems'
-
-        unless rubygems_version >= '0.9.4'
-          $stderr.puts %(Radiant requires RubyGems >= 0.9.4 (you have #{rubygems_version}). Please `gem update --system` and try again.)
+        unless rubygems_version >= min_version
+          $stderr.puts %Q(Radiant requires RubyGems >= #{min_version} (you have #{rubygems_version}). Please `gem update --system` and try again.)
           exit 1
         end
 
       rescue LoadError
-        $stderr.puts %(Radiant requires RubyGems >= 0.9.4. Please install RubyGems and try again: http://rubygems.rubyforge.org)
+        $stderr.puts %Q(Radiant requires RubyGems >= #{min_version}. Please install RubyGems and try again: http://rubygems.rubyforge.org)
         exit 1
       end
 
