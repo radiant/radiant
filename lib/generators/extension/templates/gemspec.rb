0d53cbba197f1c9c091f191ceb1969999bb067e4
Jim Gay <jim@saturnflyer.com>
2011-05-28 10:16:38 -0400

    remove git dependency on generated extension gemspecs

--- lib/generators/extension/templates/gemspec.rb
+++ lib/generators/extension/templates/gemspec.rb
@@ -11,14 +11,19 @@ Gem::Specification.new do |s|
   s.homepage    = "<%= homepage %>"
   s.summary     = %q{<%= extension_name %> for Radiant CMS}
   s.description = %q{Makes Radiant better by adding <%= file_name %>!}
-
-  s.files         = `git ls-files`.split("\n")
-  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  
+  ignores = if File.exist?('.gitignore')
+    File.read('.gitignore').split("\n").inject([]) {|a,p| a + Dir[p] }
+  else
+    []
+  end
+  s.files         = Dir['**/*'] - ignores
+  s.test_files    = Dir['test/**/*','spec/**/*','features/*'] - ignores
+  # s.executables   = Dir['bin/*'] - ignores
   s.require_paths = ["lib"]
   
   s.post_install_message = %{
   Add this to your radiant project with:
-    config.gem 'radiant-<%= file_name %>-extension', :version => '#{Radiant<%= class_name %>::VERSION}'
+    config.gem 'radiant-<%= file_name %>-extension', :version => '~>#{Radiant<%= class_name %>::VERSION}'
   }
 end
