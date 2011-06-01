113f796a48537bb5de0d7e2ced0fe78a4cb33670
Jim Gay <jim@saturnflyer.com>
2011-05-31 22:28:32 -0400

    alter compass initializer to load from tmp/stylesheets

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,8 @@
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Sass::Plugin.options[:template_location] = (Rails.root + 'public' + 'stylesheets' + 'sass').to_s
+
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
