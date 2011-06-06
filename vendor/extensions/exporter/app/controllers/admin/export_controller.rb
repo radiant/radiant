ba01839ead8cc90353b4b4ced191627898c30393
johnmuhl <git@johnmuhl.com>
2011-06-06 17:18:50 -0500

    remove vendored exporter extension, depend on gem instead

--- vendor/extensions/exporter/app/controllers/admin/export_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::ExportController < ApplicationController
-  def export
-    render :text => Radiant::Exporter.export(params[:type] || 'yaml'), :content_type => "text/yaml"
-  end
-end
--- vendor/extensions/exporter/config/routes.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.with_options(:controller => 'admin/export') do |export|
-    export.export       'admin/export',       :action => 'export'
-    export.export_type  'admin/export/:type', :action => 'export'
-  end
-end
