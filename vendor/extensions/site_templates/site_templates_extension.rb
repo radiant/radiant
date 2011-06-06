d24d7793b63ef89b2ee4083a083beab0f173ca16
johnmuhl <git@johnmuhl.com>
2011-06-06 17:34:37 -0500

    remove vendored site_templates extension, depend on gem instead

--- vendor/extensions/site_templates/site_templates_extension.rb
+++
@@ -1,8 +0,0 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application_controller'
-
-class SiteTemplatesExtension < Radiant::Extension
-  version "1.0"
-  description "Standard site templates"
-  url "http://github.com/radiant/radiant-site_templates-extension"
-end
