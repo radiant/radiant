78104f66a72f9f5f238851c2e4818a963204cb0b
johnmuhl <git@johnmuhl.com>
2011-06-06 13:18:47 -0500

    add config.gem for RedCloth

--- config/environment.rb
+++ config/environment.rb
@@ -87,7 +87,10 @@ Radiant::Initializer.run do |config|
   config.gem 'compass', :version => '~> 0.11.1'
   config.gem 'will_paginate', :version => '~> 2.3.11'
   config.gem 'delocalize', :version => '~> 0.2.3'
-  config.gem 'radiant-clipped-extension', :version => "~> 1.0.0"
+  config.gem 'paperclip', :version => '~> 2.3.3'
+  config.gem 'uuidtools', :version => "~> 2.1.2"
+  config.gem 'acts_as_list', :version => "~> 0.1.2"
+  config.gem 'RedCloth', :version => ">= 4.2.0"
 
   config.after_initialize do
     # Add new inflection rules using the following format:
