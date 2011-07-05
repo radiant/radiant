b0794450a6542d20d0a2d83b0a49e48d553cb950
johnmuhl <git@johnmuhl.com>
2011-07-05 13:27:00 -0700

    depend on the radius gem

--- config/environment.rb
+++ config/environment.rb
@@ -88,6 +88,7 @@ Radiant::Initializer.run do |config|
   config.gem "highline",      :version => "~> 1.6.2"
   config.gem "rack",          :version => "~> 1.1.1"
   config.gem "rack-cache",    :version => "~> 1.0.2"
+  config.gem "radius",        :version => "~> 0.7.0.prerelease"
   config.gem "rake",          :version => ">= 0.8.3"
   config.gem "RedCloth",      :version => ">= 4.2.0"
   config.gem "sass",          :version => "~> 3.1.1"
