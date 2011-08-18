29b2622980b49bf6fdbd5ed70ea18e505ff2cafc
johnmuhl <git@johnmuhl.com>
2011-08-17 19:03:23 -0500

    add bundler as a dependency

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -5,7 +5,7 @@ rescue LoadError
   raise "Could not load the bundler gem. Install it with `gem install bundler`."
 end
 
-if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.24")
+if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("1.0.0")
   raise RuntimeError, "Your bundler version is too old for Rails 2.3. " +
    "Run `gem install bundler` to upgrade."
 end
