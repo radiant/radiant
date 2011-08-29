94e2480fe092f08c5551041982c57e398268071e
Jim Gay <jim@saturnflyer.com>
2011-08-29 00:15:40 -0400

    bumping rc.

--- lib/radiant.rb
+++ lib/radiant.rb
@@ -6,7 +6,7 @@ unless defined? Radiant::Version
       Major = '1'
       Minor = '0'
       Tiny  = '0'
-      Patch = 'rc2' # set to nil for normal release
+      Patch = 'rc3' # set to nil for normal release
 
       class << self
         def to_s
