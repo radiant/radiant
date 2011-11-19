b5e80e04fb85266adec11b3453f0dc618fbff6a4
Benny Degezelle <hi@monkeypatch.be>
2011-11-19 16:53:37 +0100

    Fix default page status. Closes #290
    Don't allow blank for default page status.
    Use 'Draft' i.o. 'draft' in config setup.

--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -4,7 +4,7 @@ Radiant.config do |config|
   config.define 'local.timezone', :allow_change => true, :select_from => lambda { ActiveSupport::TimeZone::MAPPING.keys.sort }
   config.define 'defaults.locale', :select_from => lambda { Radiant::AvailableLocales.locales }, :allow_blank => true
   config.define 'defaults.page.parts', :default => "Body,Extended"
-  config.define 'defaults.page.status', :select_from => lambda { Status.selectable_values }, :allow_blank => true, :default => "Draft"
+  config.define 'defaults.page.status', :select_from => lambda { Status.selectable_values }, :allow_blank => false, :default => "Draft"
   config.define 'defaults.page.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
   config.define 'defaults.page.fields'
   config.define 'defaults.snippet.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
