2c6a776aa9dfd8f4ca7dc22df84ff7a70f0b62cf
Jim Gay <jim@saturnflyer.com>
2013-01-09 07:33:53 -0500

    adding Radiant.config method

--- lib/radiant.rb
+++ lib/radiant.rb
@@ -1,3 +1,10 @@
 RADIANT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) unless defined? RADIANT_ROOT
 
+module Radiant
+  def self.config
+    Radiant::Config
+  end
+end
+
 require 'radiant/engine'
+require 'radiant/version'
