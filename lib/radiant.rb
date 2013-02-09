2f4f1aea3781eaad3287d1d9611130d26e15fa36
Jim Gay <jim@saturnflyer.com>
2013-02-09 01:15:03 -0500

    bump tiny version for updates

--- lib/radiant.rb
+++ lib/radiant.rb
@@ -5,7 +5,7 @@ unless defined? Radiant::Version
     module Version
       Major = '1'
       Minor = '1'
-      Tiny  = '2'
+      Tiny  = '3'
       Patch = nil # set to nil for normal release
 
       class << self
