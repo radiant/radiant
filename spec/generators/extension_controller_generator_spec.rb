97148a195b478d4a8cfa5e71723954bb253f5194
Jim Gay <jim@saturnflyer.com>
2013-02-11 11:58:52 -0500

    ensure test directory exists for vendor/generators

--- spec/generators/extension_controller_generator_spec.rb
+++ spec/generators/extension_controller_generator_spec.rb
@@ -4,12 +4,13 @@ describe "ExtensionControllerGenerator with normal options" do
   include GeneratorSpecHelperMethods
   it_should_behave_like "all generators"
   it_should_behave_like "all extension generators"
-  
+
   before(:each) do
+    FileUtils.mkdir_p(File.join(RADIANT_ROOT, 'vendor/generators'))
     FileUtils.cp_r File.join(BASE_ROOT, 'lib/generators/extension_controller'), File.join(RADIANT_ROOT, 'vendor/generators')
     run_generator('extension_controller', %w(example Events show new index))
   end
-  
+
   it 'should generate the controller file in the correct location' do
     'vendor/extensions/example'.should have_generated_controller_for('Events') do |body|
       %w(show new index).each do |name|
@@ -17,31 +18,31 @@ describe "ExtensionControllerGenerator with normal options" do
       end
     end
   end
-  
+
   it 'should generate the controller spec file in the correct location' do
     'vendor/extensions/example'.should have_generated_controller_spec_for('Events')
   end
-  
+
   it 'should generate the helper file in the correct location' do
     'vendor/extensions/example'.should have_generated_helper_for('Events')
   end
-  
+
   it 'should generate the helper spec file in the correct location' do
     'vendor/extensions/example'.should have_generated_helper_spec_for('Events')
   end
-  
+
   it 'should generate the view files in the correct location' do
     %w(show new index).each do |action|
       'vendor/extensions/example'.should have_generated_view_for('Events', action)
     end
   end
-  
+
   it 'should generate the view spec files in the correct location' do
     %w(show new index).each do |action|
       'vendor/extensions/example'.should have_generated_view_spec_for('Events', action)
     end
   end
-  
+
   after(:each) do
     extension_dir = File.join(RADIANT_ROOT, 'vendor/extensions/example')
     FileUtils.rm_rf Dir["#{extension_dir}/app/controllers/*"]
@@ -58,12 +59,12 @@ describe "ExtensionControllerGenerator with test unit" do
   include GeneratorSpecHelperMethods
   it_should_behave_like "all generators"
   it_should_behave_like "all extension generators"
-  
+
   before(:each) do
     FileUtils.cp_r File.join(BASE_ROOT, 'lib/generators/extension_controller'), File.join(RADIANT_ROOT, 'vendor/generators')
     run_generator('extension_controller', %w(example Events show new index --with-test-unit))
   end
-  
+
   it 'should generate the controller file in the correct location' do
     'vendor/extensions/example'.should have_generated_controller_for('Events') do |body|
       %w(show new index).each do |name|
@@ -71,21 +72,21 @@ describe "ExtensionControllerGenerator with test unit" do
       end
     end
   end
-  
+
   it 'should generate the controller spec file in the correct location' do
     'vendor/extensions/example'.should have_generated_functional_test_for('Events')
   end
-  
+
   it 'should generate the helper file in the correct location' do
     'vendor/extensions/example'.should have_generated_helper_for('Events')
   end
-  
+
   it 'should generate the view files in the correct location' do
     %w(show new index).each do |action|
       'vendor/extensions/example'.should have_generated_view_for('Events', action)
     end
   end
-  
+
   after(:each) do
     extension_dir = File.join(RADIANT_ROOT, 'vendor/extensions/example')
     FileUtils.rm_rf Dir["#{extension_dir}/app/controllers/*"]
