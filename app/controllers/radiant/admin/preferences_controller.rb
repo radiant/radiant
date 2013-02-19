2de26c17554c71c3728a7bda4052ed1b4b786584
Jim Gay <jim@saturnflyer.com>
2013-02-19 00:24:23 -0500

    add missing end in preferences controller

--- app/controllers/radiant/admin/preferences_controller.rb
+++ app/controllers/radiant/admin/preferences_controller.rb
@@ -1,47 +1,48 @@
 module Radiant
-class Admin::PreferencesController < ::Radiant::AdminController
-  before_filter :load_user
+  class Admin::PreferencesController < ::Radiant::AdminController
+    before_filter :load_user
 
-  def initialize
-    @controller_name = 'user'
-    @template_name = 'preferences'
-  end
+    def initialize
+      @controller_name = 'user'
+      @template_name = 'preferences'
+    end
 
-  def show
-    set_standard_body_style
-    render :edit
-  end
-
-  def edit
-    render
-  end
-
-  def update
-    if valid_params?
-      if @user.update_attributes(params[:user])
-        redirect_to admin_configuration_path
-      else
-        flash[:error] = t('preferences_controller.error_updating')
-        render :edit
-      end
-    else
-      announce_bad_data
+    def show
+      set_standard_body_style
       render :edit
     end
-  end
 
-  private
+    def edit
+      render
+    end
 
-  def load_user
-    @user = current_user
-  end
+    def update
+      if valid_params?
+        if @user.update_attributes(params[:user])
+          redirect_to admin_configuration_path
+        else
+          flash[:error] = t('preferences_controller.error_updating')
+          render :edit
+        end
+      else
+        announce_bad_data
+        render :edit
+      end
+    end
 
-  def valid_params?
-    hash = (params[:user] || {}).symbolize_keys
-    (hash.keys - User.unprotected_attributes).size == 0
-  end
+    private
 
-  def announce_bad_data
-    flash[:error] = 'Bad form data.'
+    def load_user
+      @user = current_user
+    end
+
+    def valid_params?
+      hash = (params[:user] || {}).symbolize_keys
+      (hash.keys - User.unprotected_attributes).size == 0
+    end
+
+    def announce_bad_data
+      flash[:error] = 'Bad form data.'
+    end
   end
 end
