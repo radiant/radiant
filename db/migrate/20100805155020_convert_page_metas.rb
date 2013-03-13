003ef070997b2f5fb12a1334da8ec7fa4b09007b
Andrew vonderLuft <avonderluft@avlux.net>
2013-03-13 15:09:47 -0700

    clearing callbacks was not a great ideas.  This works better.

--- db/migrate/20100805155020_convert_page_metas.rb
+++ db/migrate/20100805155020_convert_page_metas.rb
@@ -1,12 +1,15 @@
 class ConvertPageMetas < ActiveRecord::Migration
   def self.up
-    Page.before_save.clear # no callbacks
+    # following add and remove column enables running this migration 
+    # when upgrading radiant with allowed_children_cache added to Page model
+    add_column :pages, :allowed_children_cache, :text
     Page.all.each do |page|
       page.fields.create(:name => 'Keywords', :content => page.keywords)
       page.fields.create(:name => 'Description', :content => page.description)
     end
     remove_column :pages, :keywords
     remove_column :pages, :description
+    remove_column :pages, :allowed_children_cache
   end
 
   def self.down
