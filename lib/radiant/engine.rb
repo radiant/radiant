1ae8d1218c30cdd74b87ab724263365a44406d95
Benny Degezelle <hi@monkeypatch.be>
2013-07-06 02:13:46 +0200

    Add require statements to Radiant Engine class

--- lib/radiant/engine.rb
+++ lib/radiant/engine.rb
@@ -1,4 +1,7 @@
 require 'haml'
+require 'will_paginate'
+require 'string_extensions'
+
 module Radiant
   class Engine < Rails::Engine
     isolate_namespace Radiant
@@ -8,8 +11,16 @@ module Radiant
       g.integration_tool :cucumber
     end
 
+    config.enabled_extensions = []
+
     initializer 'radiant.load_static_assets' do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+    
+    initializer 'radiant.controller' do |app|
+      ActiveSupport.on_load(:action_controller) do
+         require 'radiant/admin_ui'
+      end
     end
   end
 end
