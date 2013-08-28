d240e5c031cd662f3a783b4b1ed24855ee9a0748
Jim Gay <jim@saturnflyer.com>
2013-08-28 09:25:13 -0400

    cleanup user spec to remove user dataset, use expect syntax, remove assert_invalid assert_valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,7 @@
 require 'spec_helper'
 
 describe User, "validations" do
-  dataset :users
-  test_helper :validations
-
-  before :each do
-    @model = @user = User.new(user_params)
-  end
+  test_helper :user
 
   describe 'name' do
 
@@ -80,6 +75,13 @@ describe User, "validations" do
       user.login = 'x' * 40
       expect(user).to have(0).errors_on(:login)
     end
+
+    it 'is invalid if another User exists with the same login' do
+      user.save!
+      second = User.new(user_params)
+      expect(second.login).to be_eql(user.login)
+      expect(user.save!).to raise_error
+    end
   end
 
   describe 'password' do
@@ -113,80 +115,64 @@ describe User, "validations" do
     it "should be an array of [:name, :email, :login, :password, :password_confirmation, :locale]" do
       # Make sure we clean up after anything set in another spec
       User.instance_variable_set(:@unprotected_attributes, nil)
-      User.unprotected_attributes.should == [:name, :email, :login, :password, :password_confirmation, :locale]
+      expect(User.unprotected_attributes).to be_eql([:name, :email, :login, :password, :password_confirmation, :locale])
     end
   end
   describe "self.unprotected_attributes=" do
     it "should set the @@unprotected_attributes variable to the given array" do
       User.unprotected_attributes = [:password, :email, :other]
-      User.unprotected_attributes.should == [:password, :email, :other]
+      expect(User.unprotected_attributes).to be_eql([:password, :email, :other])
     end
-  end
-
-  it 'should validate uniqueness' do
-    assert_invalid :login, 'this login is already in use', 'existing'
-  end
-
-  it 'should validate format' do
-    assert_invalid :email, 'this is not a valid e-mail address', '@test.com', 'test@', 'testtest.com',
-      'test@test', 'test me@test.com', 'test@me.c'
-    assert_valid :email, '', 'test@test.com'
   end
 end
 
 describe User do
-  dataset :users
+  test_helper :user
 
-  before :each do
-    @user = User.new(user_params)
-  end
-
-  it 'should confirm the password by default' do
-    @user = User.new
-  end
+  let(:user){ User.new(user_params.merge(password: 'password')) }
 
   it 'should save password encrypted' do
-    @user.password_confirmation = @user.password = 'test_password'
-    @user.save!
-    @user.password.should == @user.sha1('test_password')
+    user.password_confirmation = user.password = 'test_password'
+    user.save!
+    expect(user.password).to be_eql(user.sha1('test_password'))
   end
 
   it 'should save existing but empty password' do
-    @user.save!
-    @user.password_confirmation = @user.password = ''
-    @user.save!
-    @user.password.should == @user.sha1('password')
+    user.save!
+    user.password_confirmation = user.password = ''
+    user.save!
+    expect(user.password).to be_eql(user.sha1('password'))
   end
 
   it 'should save existing but different password' do
-    @user.save!
-    @user.password_confirmation = @user.password = 'cool beans'
-    @user.save!
-    @user.password.should == @user.sha1('cool beans')
+    user.save!
+    user.password_confirmation = user.password = 'cool beans'
+    user.save!
+    expect(user.password).to be_eql(user.sha1('cool beans'))
   end
 
   it 'should save existing but same password' do
-    @user.save! && @user.save!
-    @user.password.should == @user.sha1('password')
+    user.save! && user.save!
+    expect(user.password).to be_eql(user.sha1('password'))
   end
 
   it "should create a salt when encrypting the password" do
-    @user.salt.should be_nil
-    @user.send(:encrypt_password)
-    @user.salt.should_not be_nil
-    @user.password.should == @user.sha1('password')
+    expect(user.salt).to be_nil
+    user.send(:encrypt_password)
+    expect(user.salt).to_not be_nil
+    expect(user.password).to be_eql(user.sha1('password'))
   end
 
   describe ".remember_me" do
     before do
       Radiant::Config.stub!(:[]).with('session_timeout').and_return(2.weeks)
-      @user.save
-      @user.remember_me
-      @user.reload
+      user.save
+      user.remember_me
+      user.reload
     end
 
     it "should remember user" do
-      @user.session_token.should_not be_nil
+      expect(user.session_token).to_not be_nil
     end
   end
 
@@ -194,13 +180,13 @@ describe User do
 
     before do
       Radiant::Config.stub!(:[]).with('session_timeout').and_return(2.weeks)
-      @user.save
-      @user.remember_me
+      user.save
+      user.remember_me
     end
 
     it "should forget user" do
-      @user.forget_me
-      @user.session_token.should be_nil
+      user.forget_me
+      expect(user.session_token).to be_nil
     end
   end
 
@@ -212,21 +198,21 @@ describe User, "class methods" do
   it 'should authenticate with correct username and password' do
     expected = users(:existing)
     user = User.authenticate('existing', 'password')
-    user.should == expected
+    expect(user).to be_eql(expected)
   end
 
   it 'should authenticate with correct email and password' do
     expected = users(:existing)
     user = User.authenticate('existing@example.com', 'password')
-    user.should == expected
+    expect(user).to be_eql(expected)
   end
 
   it 'should not authenticate with bad password' do
-    User.authenticate('existing', 'bad password').should be_nil
+    expect(User.authenticate('existing', 'bad password')).to be_nil
   end
 
   it 'should not authenticate with bad user' do
-    User.authenticate('nonexisting', 'password').should be_nil
+    expect(User.authenticate('nonexisting', 'password')).to be_nil
   end
 end
 
@@ -234,16 +220,16 @@ describe User, "roles" do
   dataset :users
 
   it "should not have a non-existent role" do
-    users(:existing).has_role?(:foo).should be_false
+    expect(users(:existing).has_role?(:foo)).to be_false
   end
 
   it "should not have a role for which the corresponding method returns false" do
-    users(:existing).has_role?(:designer).should be_false
-    users(:existing).has_role?(:admin).should be_false
+    expect(users(:existing).has_role?(:designer)).to be_false
+    expect(users(:existing).has_role?(:admin)).to be_false
   end
 
   it "should have a role for which the corresponding method returns true" do
-    users(:designer).has_role?(:designer).should be_true
-    users(:admin).has_role?(:admin).should be_true
+    expect(users(:designer).has_role?(:designer)).to be_true
+    expect(users(:admin).has_role?(:admin)).to be_true
   end
 end
