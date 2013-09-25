f9e54358355e1bccfb659b6404b97da237817c63
Benny Degezelle <hi@monkeypatch.be>
2013-09-26 00:46:55 +0200

    Disable eager load in the Dummy app test environment

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -7,6 +7,11 @@ Dummy::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
