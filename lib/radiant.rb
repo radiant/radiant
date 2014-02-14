6ae185d7cbbb817cac4fc9b93b7c1379f6109e3d
Benny Degezelle <hi@monkeypatch.be>
2014-02-15 00:36:11 +0100

    Make `Radiant.detail do |config| config.define ...; end` work again

--- lib/radiant.rb
+++ lib/radiant.rb
@@ -3,6 +3,7 @@ RADIANT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), "..")) unless 
 module Radiant
   class << self
     def detail
+      yield Radiant::Config if block_given?
       Radiant::Config
     end
     def configuration
