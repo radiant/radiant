78896dc79028375d8bca9dd77b29da8a322a6ea9
Arlen Walker <support@paladinweb.net>
2014-11-12 15:12:21 -0600

    Clear another rspec 3 hurdle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,8 @@ Dir[File.join(ENGINE_RAILS_ROOT, "spec/matchers/**/*.rb")].each {|f| require f }
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
+  config.infer_spec_type_from_file_location!
+
   config.before(:each, type: :controller) { @routes = Radiant::Engine.routes }
   config.before(:each, type: :routing)    { @routes = Radiant::Engine.routes }
 end
